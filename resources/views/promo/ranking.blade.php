8d4ecc55c0958ff16bb254db793522a6a30066f4
Adam Zygadlewicz <adam@zygadlewicz.com>
2017-10-31 13:47:13 +0100

    fix

--- resources/views/promo/ranking.blade.php
+++ resources/views/promo/ranking.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 
 
-@include('shared.did-you-know', ["title"=>"Oto zaangażowanie w promocję udziału w Targach w ostatnich 3 miesiącach", "description"=>"Targi to wspólne dzieło!  Świadczenia takie jak ekskluzywny wywiad video, dostęp do aplikacji mobilnej do zbierania leadów dostępne są tylko dla najbardziej zaangażowanych Wystawców."])
+@include('shared.did-you-know', ["title"=>"Zaangażowanie w promocję udziału w Targach w ostatnich 3 miesiącach (liczba użytkowników odwiedzających stronę Targi eHandlu)", "description"=>"Targi to wspólne dzieło!  Świadczenia takie jak ekskluzywny wywiad video, dostęp do aplikacji mobilnej do zbierania leadów dostępne są tylko dla najbardziej zaangażowanych Wystawców."])
 
 
 <div id="ranking"></div>
