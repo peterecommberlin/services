2ab8d3075143fa1935a58ba288c1291181c7dc98
Adam Zygadlewicz <adam@zygadlewicz.com>
2017-11-01 14:45:51 +0100

    fb js sdk

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -27,6 +27,30 @@
   </head>
 
   <body>
+
+
+<script>
+window.fbAsyncInit = function() {
+FB.init({
+appId            : '{{config("promo.fb_app_id", "")}}',
+autoLogAppEvents : true,
+xfbml            : true,
+version          : 'v2.10'
+});
+FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) {return;}
+js = d.createElement(s); js.id = id;
+js.src = "https://connect.facebook.net/en_US/sdk.js";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+
+
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         
