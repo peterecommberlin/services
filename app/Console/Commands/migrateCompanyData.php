46cec58313668467debd43ad1782e1d10489c251
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-03-15 00:49:39 +0100

    company data migration routine

---
+++ app/Console/Commands/migrateCompanyData.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use Eventjuicer\Services\Resolver;
+use Eventjuicer\Repositories\ParticipantRepository;
+use Eventjuicer\Repositories\Criteria\BelongsToEvent;
+use Eventjuicer\Repositories\Criteria\ColumnGreaterThan;
+use Eventjuicer\Services\CompanyData;
+
+class migrateCompanyData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'companies:migrate {host}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'migrate data from participant profile to company data model';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(ParticipantRepository $p, CompanyData $cd)
+    {
+        $route = new Resolver($this->argument("host"));
+
+        $eventId =  $route->getEventId();
+
+        $this->info("Event id: " . $eventId);
+
+        //fetch all participants with company_id > 0
+
+        $p->pushCriteria(new BelongsToEvent($eventId));
+        $p->pushCriteria(new ColumnGreaterThan("company_id", 0));
+        $p->with(["fields"]);
+
+        $participants = $p->all();
+
+        $this->info($participants->count() . " participants found.");
+
+        foreach($participants as $participant)
+        {
+           $fieldsUpdated = $cd->migrate($participant);
+
+           $this->line("Processing: " . $participant->email);
+
+           if( is_array($fieldsUpdated) && count($fieldsUpdated) )
+            {
+                $this->info(implode(",", $fieldsUpdated) . " updated.");
+            }
+            else
+            {
+                $this->error("skipped...");
+            }
+        }
+
+    }
+}
