710d88706c3925b7c4e74845c2cc0ee0cf873024
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-09-15 21:21:35 +0200

    forcing redis sock

--- config/database.php
+++ config/database.php
@@ -118,10 +118,12 @@ return [
         'client' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            // 'host' => env('REDIS_HOST', '127.0.0.1'),
+            // 'password' => env('REDIS_PASSWORD', null),
+            // 'port' => env('REDIS_PORT', 6379),
             'database' => 0,
+            'scheme'     => 'unix',
+            'path'     => '/var/run/redis/redis.sock',
         ],
 
     ],
