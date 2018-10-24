e3788cae696bc04d86f1249e5e134fb9066244f8
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-10-25 01:42:20 +0200

    promoninja in english FTW

--- resources/views/emails/company/promoninja-intro-en.blade.php
+++ resources/views/emails/company/promoninja-intro-en.blade.php
@@ -4,97 +4,102 @@
  
 # Hi {{$profile->translate("[[fname]]")}},
 
-**We have enabled a challenge/competition in your exhibitor's account**
+**We have enabled a challenge / competition in your exhibitor's account**
 
-Ale o co chodzi?
+What's the idea behind it?
 
 @component('mail::panel')
 
- Część dodatkowych usług i świadczeń jest dostępna tylko dla Wystawców, którzy zapraszają swoich Klientów i Partnerów do odwiedzenia stoiska Twojej firmy na Targach. Wiekszość tych świadczen jest bardzo cenna i nie można ich kupić. **Wartość świadczeń to ponad 50 000 pln **
+Some of our premium services are available only for Exhibitors that are promoting their participation in the Expo. 
+
+We believe that a successful event is not only getting to know new customers, but also strengthening relationships with existing clients.
+
+The market value of the services we put to the challenge is higher that 15 000 EUR.
 
 @endcomponent
 
-Wyzwanie polega na posługiwaniu się ** "specjalnym linkiem zliczającym punkty"** lub indywidualnymi materiałami promocyjnymi, które przygotowaliśmy i które już zawierają w sobie ten link.
+The Challenge consists in using special link which give you points.
 
-
-Oto Twój **link trackujący!** Wystarczy, że Twoje newslettery, stopki @, reklamy, posty w serwisach społecznościowych będą linkowały do Twojego profilu na stronie Targów eHandlu za pomocą poniższego linka i bierzesz udział w Wyzwaniu.
+Below please find your special link to your company profile on expo website. Use it when creating newsletters, email footers, advertisement (fb ads, adwords) or directly when informing / promoting on social websites. 
 
 @component('mail::panel')
 [{{ $trackingLink }}]({{ $trackingLink }}) 
 @endcomponent
 
-**UWAGA: Najważniejsze nagrody - 2 prezentacje - zostaną przyznane w środę (31 października) o godzinie 12:00 na podstawie stanu rankingu z tej godziny, reszta konkursu trwa aż do 6 listopada**
 
-# W Panelu Wystawcy czekają na Ciebie 4 ważne strony 
+**IMPORTANT: The most precious rewards - 2 presentation slots - will be assigned on Wednesday - 31 OCT at 12:00 CET. The rest of the competition will be enabled till 6 NOV 12:00 CET**
+
+# In your Exhibitor's account you will find 4 important sections related to the Challenge.
 
 @component('mail::button', ['url' => $accountUrl])
-Zaloguj się do Panelu Wystawcy 
+Sign In
 @endcomponent
 
 # RANKING
 
-Ranking tworzony jest **z opóźnieniem ok 24h** na podstawie danych z Google Analytics.
-Codziennie będziesz mógł zobaczyć jaki jest wynik Twoich i innych Wystawców działań z dnia poprzedniego.
+The ranking is created on the basis of data from google analytics with a **24-hour delay.**
+When you check the results, you actually see the results from the previous day.
 
-## NAGRODY
+## REWARDS
 
-Każda nagroda ma warunki jej przyznania: 
+Each of the prizes requires fulfillment of the conditions for its granting.
 
-* co do minimalnej liczby punktów 
-* co do pozycji w rankingu. Przy każdej pozycji nagrody widzisz czy biorąc pod uwagi stan punktów z dnia poprzedniego miał(a)byś ją przyznaną.
+* minimal number of points (= pageviews of your public exhibitor's profile)
+* position in the ranking
 
-## SZABLONY NEWSLETTERA
+Looking at every listed prize you can see whether your company meets the conditions or not. Please remember that everything may change till the end of the Challenge so it is recommended not to open the champagne until the end of the competition.
 
-2 szablony (polski i angielski), które informują o tym, że Twoja firma będzie na Targach eHandlu. Szablony mają link, który prowadzi do Twojego profilu i zlicza punkty.
-Każdy newsletter możesz pobrać jako **paczkę ZIP i wczytać do programów typu Freshmail, GetResponse, Mailchimp**...
+## EMAIL TEMPLATES
 
-## MATERIAŁY PROMOCYJNE
+2 ready-to-send newsletters (polish and english), which are designed to inform your clients and partners about your participation in the event. You can copy HTML or download a zip file that can be easily imported by GetResponse / Mailchimp-like software.
 
-To 3 oddzielne linki, które możesz udostępniać na serwisach społecznościowych. każdy z nich generuje inną grafikę jako podgląd (2 z Twoim logotypem... i jedna to Twoja własna kreacja jeśli podano URL do niej w Danych firmy).
+## PROMOTIONAL MATERIALS
 
-Każdy z linków do grafiki możesz skopiować do schowka i wysłać via @, messenger, dodać do własnego newslettera.
+3 separate links, which you can share on social websites will render different image
 
-@component('mail::button', ['url' => $accountUrl])
-Zaloguj się do Panelu Wystawcy 
-@endcomponent
+* your logotype on our background with polish invitation
+* as above but with english
+* custom design (you will need to give full URL to your design in Company Data)
 
-# Wybrane nagrody
-
-## 2 Prezentacje na jednej z 2 głównych scen
-
-15-minutowy slot prezentacyjny podczas wydarzenia. Twoja prezentacja zostanie dodana do oficjalnej agendy Targów eHandlu.
-
-## Wywiad wideo z przedstawicielem firmy na stoisku
-
-Zostanie on przeprowadzony podczas wydarzenia, a następnie opublikowany na fanpage'u Targów eHandlu oraz portalu ehandelmag. Oczywiście zostanie też przekazany do dowolnego wykorzystania przez Wystawcę :)
-
-## Dostęp do najtańszej puli sprzedaży na kolejne Targi
-
-Kolejne Targi eHandlu już w kwietniu w Krakowie. Najbardziej zaangażowani Wystawcy dostaną informację apropos otwarcia sprzedaży wcześniej. Niższe ceny, lepszy wybór lokalizacji stoiska.
-
-## Wyróżnienie logotypu
-
-Twój logotyp zostanie wyświetlony na oficjalnej stronie Targów eHandlu wśród wiodących firm z branży e-commerce przez okres 6mcy!
-
-## Aplikacja do skanowania identyfikatorów Zwiedzająych i dane kontaktowe w excelu
-
-Otrzymasz dostęp do aplikacji umożliwiającej skanowanie identyfikatorów zwiedzających. Każdy z przedstawicieli Twojej firmy będzie mógł zbierać dane o odwiedzających i eksportować wyniki do .xls lub .csv.
-
-## Roll-up w strefie cateringu dla Zwiedzających
-
-Twój firmowy roll-up znajdzie się w strefie gastronomicznej dla Zwiedzających.
+Every link can be copied to clipboard, sent via messenger / @ or used in your designed-from-scratch newsletters.
 
 @component('mail::button', ['url' => $accountUrl])
-Zaloguj się do Panelu Wystawcy 
+Sign In  
 @endcomponent
 
-## Chcesz użyć własnych materiałów promocyjnych? Użyj linku poniżej: 
+# Some of the PRIZES
+
+## Presentation slot on one of the 2 main stages
+
+15-minute presentation slot during upcoming expo. Your presentation will be added to the official agenda and promoted. (That is the reason that this prize will be assigned 31 OCT)
+
+## VIDEO interview
+
+Your company will be video-interviewed at your booth during Expo! You will own the resulting material and also can expect we will promote it after the event. 
+
+## Access to the Early Bird Sales 
+
+Next edition of E-commerce Poland Expo will be held in Cracow in April 2019. Only 30 most ranked companies will benefit from lowest prices and best locations.
+
+## Promotion of the company logo on our website
+
+Your logotype will be promoted during next 5 months - till the next event.
+
+## Mobile app to scan visitors' badges
+
+All your representatives can install iOS / Android apps allowing them to scan visitors' badges (tickets) and collect their contact details. You will be able to download XLS file directly from Exhibitor's account with all scanned visitors from all your connected devices.
+
+## Roll-up in catering zone
+
+Bring your roll-up and place it among visitors in their catering zone. Have some additional traffic at your booth.
+
+## Want to use your own promo materials? Great. Always use the provided link!
 
 @component('mail::panel')
 [{{ $trackingLink }}]({{ $trackingLink }}) 
 @endcomponent
 
-Pozdrawiam, 
+Regards, 
 
 Adam Zygadlewicz
 
