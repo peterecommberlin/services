36f0056e13104570c7df3d9770a7e593cd915d3a
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-10-31 14:45:38 +0100

    logistics en

--- resources/views/emails/company/logistics-en.blade.php
+++ resources/views/emails/company/logistics-en.blade.php
@@ -2,105 +2,79 @@
 
 @component('mail::message')
  
-# Hi {{$profile->translate("[[fname]]")}},
+# Hello {{$profile->translate("[[fname]]")}},
 
-**We have enabled a challenge / competition in your exhibitor's account**
+# Here you have basic logistic information about Ecommerce Warsaw Expo:
 
-What's the idea behind it?
-
-@component('mail::panel')
-
-Some of our premium services are available only for Exhibitors that are promoting their participation in the Expo. 
-
-We believe that a successful event is not only getting to know new customers, but also strengthening relationships with existing clients.
-
-The market value of the services we put to the challenge is higher that 15 000 EUR.
-
-@endcomponent
-
-The Challenge consists in using special link which give you points.
-
-Below please find your special link to your company profile on expo website. Use it when creating newsletters, email footers, advertisement (fb ads, adwords) or directly when informing / promoting on social websites. 
+## Oversized parcels and materials which you want to ship and have delivered directly to the booth. Recipient:
 
 @component('mail::panel')
-[{{ $trackingLink }}]({{ $trackingLink }}) 
+
+ExpoSped Sp. z o.o.
+Prądzyńskiego Street 12/14, 
+01-222 Warszawa,
+Contact person: Piotr Kopański / +48 504103570
+
+Please describe parcel as 'XV Targi eHandlu [number of booth OR name of the company'
+
+Parcel can reach the place no earlier than November 2! All parcels will be delivered to the Fair on setup day - 06th of November.
+
+
 @endcomponent
 
 
-**IMPORTANT: The most precious rewards - 2 presentation slots - will be assigned on Wednesday - 31 OCT at 12:00 CET. The rest of the competition will be enabled till 6 NOV 12:00 CET**
+## Place:
 
-# In your Exhibitor's account you will find 4 important sections related to the Challenge.
+EXPO XXI - Prądzyńskiego Street 12/14, 01-222 Warsaw
 
-@component('mail::button', ['url' => $accountUrl])
-Sign In
-@endcomponent
+## Parking
 
-# RANKING
+In the standard offering (without buying additional ones) you have a one parking card, which is valid from the moment of entering the EXPO area to end of the event.
 
-The ranking is created on the basis of data from google analytics with a **24-hour delay.**
-When you check the results, you actually see the results from the previous day.
+The parking card will be in your "welcome pack" on your booth, and you need it only for leaving.
 
-## REWARDS
+If the main parking will be full we will open the parking on the other side of the street.
 
-Each of the prizes requires fulfillment of the conditions for its granting.
 
-* minimal number of points (= pageviews of your public exhibitor's profile)
-* position in the ranking
+## Assembly day 
 
-Looking at every listed prize you can see whether your company meets the conditions or not. Please remember that everything may change till the end of the Challenge so it is recommended not to open the champagne until the end of the competition.
+* 6th of November from 4:00 PM to 8:00 PM.
+* 7th of November from 8:00 AM to 9:30 AM.
 
-## EMAIL TEMPLATES
+After entering the hall you need to go to the information point for the exhibitors. Your Account Manager will await :)
 
-2 ready-to-send newsletters (polish and english), which are designed to inform your clients and partners about your participation in the event. You can copy HTML or download a zip file that can be easily imported by GetResponse / Mailchimp-like software.
+At your booth, you will find a welcome pack including: 
 
-## PROMOTIONAL MATERIALS
+* the badges of your representatives if you have added them before the deadline at your exhibitor's panel, 
+* lunch vouchers, 
+* contact numbers to the staff of Expo, 
+* parking card
+* any additionally paid vouchers, parking cards...
 
-3 separate links, which you can share on social websites will render different image
+** During the assembly day (6th of November) the hall would not be heated, because of that, we ask you to be prepared and take warm clothes with you. **
 
-* your logotype on our background with polish invitation
-* as above but with english
-* custom design (you will need to give full URL to your design in Company Data)
+** Please take your own power extension cord / bar **
 
-Every link can be copied to clipboard, sent via messenger / @ or used in your designed-from-scratch newsletters.
+** Remember, that standard power access is only 300 Watts ** 
+If you plan to use something more that, for instance, 2 laptops and TV you need to buy an extra package (3000 Watts).  If you want to consult power usage of your equipment, please contact with us.
 
-@component('mail::button', ['url' => $accountUrl])
-Sign In  
-@endcomponent
+## Expo day
 
-# Some of the PRIZES
+7th of November from 10.00 AM to 5.00 PM.
 
-## Presentation slot on one of the 2 main stages
+In case of any problem (e.g., with electricity) you should go to the information for exhibitors or call your Account Manager, whose number you will find in the welcome pack.
 
-15-minute presentation slot during upcoming expo. Your presentation will be added to the official agenda and promoted. (That is the reason that this prize will be assigned 31 OCT)
+** At 5.00 PM we invite you to the networking area for a celebration. **
 
-## VIDEO interview
+## Disassembly
 
-Your company will be video-interviewed at your booth during Expo! You will own the resulting material and also can expect we will promote it after the event. 
-
-## Access to the Early Bird Sales 
-
-Next edition of E-commerce Poland Expo will be held in Cracow in April 2019. Only 30 most ranked companies will benefit from lowest prices and best locations.
-
-## Promotion of the company logo on our website
-
-Your logotype will be promoted during next 5 months - till the next event.
-
-## Mobile app to scan visitors' badges
-
-All your representatives can install iOS / Android apps allowing them to scan visitors' badges (tickets) and collect their contact details. You will be able to download XLS file directly from Exhibitor's account with all scanned visitors from all your connected devices.
-
-## Roll-up in catering zone
-
-Bring your roll-up and place it among visitors in their catering zone. Have some additional traffic at your booth.
-
-## Want to use your own promo materials? Great. Always use the provided link!
-
-@component('mail::panel')
-[{{ $trackingLink }}]({{ $trackingLink }}) 
-@endcomponent
+7th of November. Disassembly is possible from 5.15 PM to 8.00 PM
 
 Regards, 
 
+Marta Zaczyk
+Jakub Przybylski
+Jan Selga
 Adam Zygadlewicz
 
 @endcomponent
