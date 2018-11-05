cf725f08c0ab1e5cddaaf3e900819d3c79b59c6b
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-11-05 16:14:55 +0100

    logistics reminder

---
+++ resources/views/emails/company/logistics-reminder-pl.blade.php
@@ -0,0 +1,71 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{$profile->translate("[[fname]]")}},
+
+# Oto podstawowe informacje logistyczne:
+
+## Miejsce
+
+EXPO XXI - ul. Prądzyńskiego 12/14, 01-222 Warszawa
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/v1541430466/Teren_EXPO_XXI_hale1234_-_2013.08.21_-_bez_siatki.pdf_1_page_2018-11-05_15-55-44.jpg" alt="mapka / mapa" style="max-width: 650px; "/>
+
+## Parking
+
+o ile nie została dokupiona dodatkowa, standardowo mają Państwo do dyspozycji jedną kartę parkingową (per stoisko), która jest ważna od momentu wjazdu do momentu zakończenia targów
+
+karta parkingowa będzie znajdowała się w "welcome packu" na stoisku i **potrzebna jest tylko do wyjazdu / wyjazdów **
+
+po zapełnieniu się głównego parkingu EXPO zostanie uruchomiony parking po drugiej stronie ulicy. niezależnie od dział operatora EXPO XXI będziemy starali się zarządzać ruchem samochodowym
+
+## Montaż 
+
+* 6 listopada w godzinach 16:00 - 20:00
+* 7 listopada od 8:00 do 9:30
+
+**Rozładunek samochodów tylko przez bramę wskazaną na mapce powyżej**
+
+Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców, na którym będzie czekał Państwa Opiekun i który zaprowadzi Państwa do właściwego stoiska.
+
+Na państwa stoiskach znajdować będzie się pakiet powitalny w którego skład wchodzą: 
+
+* identyfikatory przedstawicieli jeśli zostali dodani w Panelu Wystawcy, 
+* vouchery obiadowe, 
+* numery kontaktowe do osób z obsługi targów, 
+* karta parkingowa (potrzebna do wyjazdu w dzień montażowy i wyjazdu po wydarzeniu)
+
+** W dzień montażowy Hala nie będzie ogrzewana - bardzo proszę o wzięcie ciepłej odzieży**
+
+** Prosimy o wzięcie własnych listew zasilających **
+
+** Prosimy pamiętać, że standardowe przyłącze prądu to tylko 300W** jeśli planujesz coś więcej niż korzystanie z 2 laptopów i ew. TV trzeba dokupić indywidualne przyłącze. Prosimy o kontakt jeśli masz wątpliwości.
+
+@component('mail::panel')
+
+UWAGA: W przypadku podklejania wykładzin we własnym zakresie (brak wykupienia stosownej usługi od nas) jedyną rekomendowaną opcją jest profesjonalna taśma dwustronna ETG firmy Imawex. Typowa (=marketowa) taśma dwustronna jest nie do usunięcia po wydarzeniu co generuje dodatkowe koszty sprzątania.
+
+@endcomponent
+
+## Wydarzenie Targowe 
+
+7 listopada 10:00 do 17:00. 
+
+W razie wystąpienia jakichkolwiek problemów związanych z Państwa stoiskiem (np z prądem) należy udać się do informacji dla wystawców lub dzwonić na numer do Opiekuna, który znajduje się w kopercie / pakiecie powitalnym.
+
+** O 17:00 zapraszamy na toast do strefy networkingu.**
+
+## Demontaż
+
+7 listopada. Demontaż możliwy jest w godzinach od 17.15 do 20:00
+
+Regards, 
+
+Marta Zaczyk
+Jakub Przybylski
+Jan Selga
+Adam Zygadlewicz
+
+@endcomponent
+
