22f6e083a5de0f28944cb5a9c42d0c850a23ece5
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-11-05 16:27:20 +0100

    logistics reminder en

---
+++ resources/views/emails/company/logistics-reminder-en.blade.php
@@ -0,0 +1,69 @@
+
+
+@component('mail::message')
+ 
+# Hello {{$profile->translate("[[fname]]")}},
+
+# Here you have basic logistic information about Ecommerce Warsaw Expo:
+
+## Place:
+
+EXPO XXI - Prądzyńskiego Street 12/14, 01-222 Warsaw
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/v1541430466/Teren_EXPO_XXI_hale1234_-_2013.08.21_-_bez_siatki.pdf_1_page_2018-11-05_15-55-44.jpg" alt="mapka / mapa" style="max-width: 650px; "/>
+
+
+## Parking
+
+In the standard offering (without buying additional one(s) ) you have a one parking card, which is valid from the moment of entering the EXPO area to end of the event.
+
+The parking card will be in your "welcome pack" on your booth, and you need it **only for leaving**
+
+If the main parking will be full we will open the parking area on the other side of the street.
+
+## Assembly day 
+
+* 6th of November from 4:00 PM to 8:00 PM.
+* 7th of November from 8:00 AM to 9:30 AM.
+
+If you want to unload your stuff and are coming by car - please use the unloading gate shown on the map.
+
+After entering the hall you need to go to the information point for the exhibitors. Your Account Manager will await :)
+
+At your booth, you will find a welcome pack including: 
+
+* the badges of your representatives if you have added them before the deadline at your exhibitor's panel (if not as us for blank ones), 
+* lunch vouchers, 
+* contact numbers to the staff of Expo, 
+* parking card
+* any additionally paid vouchers, parking cards...
+
+** During the assembly day (6th of November) the hall would not be heated, because of that, we ask you to be prepared and take warm clothes with you. **
+
+** Please take your own power extension cord / bar ** 
+
+** Remember, that standard power access is only 300 Watts ** 
+
+If you plan to use something more that, for instance, 2 laptops and TV you need to buy an extra package (3000 Watts).  If you want to consult power usage of your equipment, please contact with us.
+
+## Expo day
+
+7th of November from 10.00 AM to 5.00 PM.
+
+In case of any problem (e.g., with electricity) you should go to the information for exhibitors or call your Account Manager, whose number you will find in the welcome pack.
+
+** At 5.00 PM we invite you to the networking area for a celebration. **
+
+## Disassembly
+
+7th of November. Disassembly is possible from 5.15 PM to 8.00 PM
+
+Regards, 
+
+Marta Zaczyk
+Jakub Przybylski
+Jan Selga
+Adam Zygadlewicz
+
+@endcomponent
+
