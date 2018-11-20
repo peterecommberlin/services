5bf85bc9c90c01ac3e74ee4b32b6b5bb2bbcb625
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-11-20 15:42:56 +0100

    poprawka linków

--- resources/views/emails/company/outro-photos-en.blade.php
+++ resources/views/emails/company/outro-photos-en.blade.php
@@ -6,11 +6,11 @@
 
 ** Below you can find a link to gallery of photos from recent E-commerce Warsaw Expo event.**  Photos have been divided into folders (by exhibitor's brand name) to save your time :)  
 
-@component('mail::button', ['url' => "https://goo.gl/forms/R6OrYXvhPTGv4uUm1"])
+@component('mail::button', ['url' => "https://files.targiehandlu.pl/teh15/"])
 Browse photos
 @endcomponent
 
-We kindly remind to [fill in satisfaction survey](https://files.targiehandlu.pl/teh15/) if we do not have your answers.
+We kindly remind to [fill in satisfaction survey](https://goo.gl/forms/R6OrYXvhPTGv4uUm1) if we do not have your answers.
 
 Regards, Adam Zygadlewicz
 
--- resources/views/emails/company/outro-photos-pl.blade.php
+++ resources/views/emails/company/outro-photos-pl.blade.php
@@ -6,11 +6,11 @@
 
 ** Oto Galeria zdjęć z Targów eHandlu z podziałem na firmy Wystawców** 
 
-@component('mail::button', ['url' => "https://goo.gl/forms/R6OrYXvhPTGv4uUm1"])
+@component('mail::button', ['url' => "https://files.targiehandlu.pl/teh15/"])
 Przeglądaj folder
 @endcomponent
 
-Proszę Cię o [wypełnienie ankiety satysfakcji](https://files.targiehandlu.pl/teh15/) jeśli jeszcze nie mamy Twoich odpowiedzi na nurtujące nas pytania.
+Proszę Cię o [wypełnienie ankiety satysfakcji](https://goo.gl/forms/R6OrYXvhPTGv4uUm1) jeśli jeszcze nie mamy Twoich odpowiedzi na nurtujące nas pytania.
 
 PS. Mamy tylko **49 wypełnionych** ankiet.
 
