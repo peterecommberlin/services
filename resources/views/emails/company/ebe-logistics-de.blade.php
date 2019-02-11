24e16c36173074d2245a792395782efc153825cb
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-02-12 00:24:18 +0100

    ebe @ logistics

---
+++ resources/views/emails/company/ebe-logistics-de.blade.php
@@ -0,0 +1,74 @@
+
+
+@component('mail::message')
+ 
+# Hello {{$profile->translate("[[fname]]")}},
+
+# Hier findest du Infos rund um die Logistik für die anstehende E-commerce Berlin 2019 Expo. 
+
+# Vorab, Lieferungen von großen Gegenständen können zum Veranstaltungsort geschickt werden. Sie werden auch auf der Standfläche warten. Folgende Informationen müssen die Lieferungen enthalten: 
+
+@component('mail::panel')
+
+Empfänger: 
+
+STATION Berlin, Luckenwalder Str. 4-6, 10963 Berlin 
+
+℅ Nadin Geyer, E-commerce Berlin [Ausstellername oder Standnummer]
+
+Die Pakete können ab dem 19.02.19 in der STATION Berlin ankommen. 
+
+@endcomponent
+
+Kleinere Pakete werden bei den Infopunkten auf dem Messegelände warten und können abgeholt werden. 
+
+## Ort:
+
+STATION Berlin, Luckenwalder Str. 4-6, 10963 Berlin 
+
+# Parksituation: 
+
+Das Parken auf dem Gelände der Expo ist am Tag der Expo nicht möglich. 
+Neben dem Expogelände gibt es ein Parkhaus, welches benutzt werden kann. Das Tagesticket kostet 10€. 
+
+Ihr könnt am Aufbautag (19.02.2019) mit den Autos auf dem Gelände vor den Hallen parken.
+
+Wenn deine Ausstellungsfläche in der ersten Halle ist (rechts auf der Mappe), dann kannst du von der Luckenwalder Straße aus reinfahren und dort parken. 
+Wenn deine Ausstellungsfläche in der zweiten Halle ist (links auf der Mappe) dann kannst du von der Trebbine Straße aus reinfahren und dort parken. 
+
+Beim Reinfahren wird eine Kaution iHv. 50€ für PKW’s und 100€ für LKW’s genommen. Die Kaution wird zurückgezahlt beim Rausfahren während des Aufbautages. 
+
+
+## Aufbautage:
+
+**19.02.2019 14-24 Uhr** und **20.02.2019 8-9:30 Uhr**
+
+Seid ihr in euren Hallen angekommen, begebt euch bitte zum Infopunkt. Es wird zwei geben (1x pro Halle). Dort wird euer Account Manager auf euch warten. Dort bekommt ihr euer Willkommensbag ausgehändigt, was folgendes enthält: 
+
+* Ausstellerpässe der registrierten Vertreter 
+* Catering Voucher 
+* WLAN-Zugriff (Login-Daten) 
+* Kontaktdaten des Expoteams vor Ort 
+
+Wichtig! Jede Flächenkategorie innerhalb der 9m2 haben 500W Strom. Die Grandkategorie hat 1000W. 
+
+Falls ihr mehr braucht um andere Geräte anzuschließen, dann meldet euch schnellstmöglich bei unserem Team um über das Upgrade zu sprechen. 
+
+## Expotag: 
+
+20.02.2019 von 10-17 
+
+Sollte es Probleme mit dem Strom oder anderem geben, gebt eurem Account Manager bescheid. Die Kontaktdaten werden im Willkommensbage sein. 
+
+## Abbau: 
+
+20.02.2019 17:15-24:00 
+
+Wenn ihr Sachen wieder einladen möchtet in eure PKW’s/LKW’s, dann könnt ihr erst ab 18 Uhr in die jeweiligen Bereich reinfahren. Die Regeln vom Aufbautag gelten hier dementsprechend. 
+
+Regards, 
+
+E-commerce Berlin Expo Team
+
+@endcomponent
+
---
+++ resources/views/emails/company/ebe-logistics-en.blade.php
@@ -0,0 +1,89 @@
+
+
+@component('mail::message')
+ 
+# Hello {{$profile->translate("[[fname]]")}},
+
+# Here you have basic logistic information about E-commerce Berlin Expo:
+
+## Oversized parcels and materials which you want to ship and have delivered directly to the booth. Recipient:
+
+@component('mail::panel')
+
+STATION Berlin Luckenwalder Str 4-6 
+10963 Berlin
+
+Contact person: Nadin Geyer
+
+Please describe parcel as 'E-commerce Berlin 2019 Expo [number of the booth OR name of the company]'
+
+Parcel can reach the place no earlier than February, the 18th! All parcels will be available at two info points at the exhibition hall. You can collect them there. 
+
+@endcomponent
+
+## Place:
+
+STATION Berlin  - Luckenwalder Str. 4-6, 10963 Berlin 
+
+## Parking during assembly day (Tuesday / 19 Feb)
+
+You can park your car at the venue during the assembly day in front of the restrictive halls. 
+
+** If your exhibition space is at hall 1 (right floor on the map), you can park your car entering from Luckenwalder Str. **
+
+** If your exhibition space is in hall 2 (the left one on the map) you can park your car entering from Trebbiner Str.**
+
+** The venue owner will take a deposit for cars worth 50€ and for trucks 100€. The deposit will be given back when you leave the venue during the assembly day. **
+
+## Parking during EXPO day (Wednesday / 20 Feb)
+
+Parking during the Expo day is **not possible on the area of the venue. **
+
+Next to the STATION Berlin, there is a multilevel parking house which can be used during the Expo day. 
+
+The one-day-ticket is 10€ worth. 
+
+
+## Assembly day 
+
+* 19th of February from 2:00 PM to Midnight
+* 20th of February from 8:00 AM to 9:30 AM.
+
+
+After entering the hall you need to go to the information point for the exhibitors.
+
+There will be two of them - one in each hall. Your Account Manager will await :)
+
+There, you will get your exhibitor welcoming package which includes: 
+
+* the badges of your representatives if you have added them before the deadline at your exhibitor's panel,
+* lunch vouchers,
+* internet network access - login data will be included. 
+* contact numbers to the staff of Expo,
+
+
+** Remember, that standard power access is:** 
+a) only 500 Watts (for the categories Start-Up, Hot/Super Hot). b) The grand space has 1000 Watts. 
+
+If you plan to use something more and it needs more electricity, please contact us as soon as possible. If you want to consult power usage of your equipment, please contact us.
+
+
+## Expo day
+
+20th of February from 10.00 AM to 5.00 PM.
+
+In case of any problem (e.g., with electricity) you should go to the information for exhibitors or call your Account Manager, whose number you will find in the welcome pack.
+
+## Disassembly
+
+20th of November. Disassembly is possible from 5.15 PM to 10.00 PM. If you need to upload your car with your stuff, you will be able to drive on the venue from 6.00 PM, not earlier. 
+
+The same rules are valid as mentioned in the assembly day section. 
+
+
+Regards, 
+
+E-commerce Berlin Expo Team
+
+@endcomponent
+
