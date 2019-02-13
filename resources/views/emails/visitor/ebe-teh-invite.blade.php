6b6f3944f350f47f1f7e62a3f1ffa2b05f0af6ff
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-02-13 15:32:28 +0100

    ebe teh

---
+++ resources/views/emails/visitor/ebe-teh-invite.blade.php
@@ -0,0 +1,118 @@
+
+
+@component('mail::message')
+
+<img src="http://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1523951554/og_teh_default.jpg" style="margin: 10px auto;" alt="logotyp Targów eHandlu" />
+
+# Cześć {{$p->translate("[[fname]]") }}!
+
+Najbliższe Targi eHandlu odbędą się **17 kwietnia w Krakowie.** Ta właśnie edycja Targów ma szansę stać się edycją międzynarodową i będziemy mocno o to zabiegać! :)
+
+Planujemy aż **4 sceny prezentacyjne** i rekordową liczbę Wystawców - [to **ostatnie 2 dni aktualnej puli sprzedaży stoisk!**](https://targiehandlu.pl/exhibit?utm_campaign=ebe).
+
+<a href="https://targiehandlu.pl/exhibit?utm_campaign=ebe" target="_blank">
+	<img src="https://res.cloudinary.com/eventjuicer/image/uploadc_fit,w_650/v1550057962/16_Targi_eHandlu_Krako%CC%81w_17_04_2018_2019-02-13_12-35-00.jpg" alt="" />
+</a>
+
+Wcześniej bo już **w przyszłym tygodniu możemy zobaczyć się E-commerce Berlin Expo**, które mamy zaszczyt współorganizować. 
+
+Wydarzenie jest przeznaczone dla Profesjonalistów, którzy myślą o handlu międzynarodowym, skalowaniu i automatyzacji. Wydarzenie jest oczywiście anglojęzyczne.
+
+# Hi E-commerce Professional!
+
+Join us on **20th of February at STATION Berlin** (Luckenwalder Str. 4-6) during E-commerce Berlin Expo.
+
+Meet over **5000 international participants**, including industry leaders like:
+
+**Swatch, Zalando, Mister Spex, myToys, kfzteile24, Moebel.de, Otto, Idealo, Intershop, Avalara, Kenshoo, Youtube, Google, Facebook, MasterCard** 
+
+Listen to killer speeches and learn how to go multichannel, optimize conversions and automate almost every part of e-commerce business!
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/visit?utm_campaign=teh"])
+Register here for E-commerce Berlin Expo
+@endcomponent
+
+# Please find official agenda below!
+
+**E-Commerce Trends 2019** – fear and desire of German online-shoppers - Erik Meierhoff **idealo**
+
+The Power of Social: How to **build your brand** without breaking the bank **by creating viral content** instead - Christoph von Bülow **HolidayPirates**
+
+Pushing creative boundaries within the mobile space - Christina Keller **Facebook**
+
+**Blockchain for E-Commerce**- everything retailers could do and what they really should do - Constance Stein **Otto Group**
+
+From a monolithic desktop focused legacy architecture to a flexible, mobile first and **agile micro service solution at moebel.de** (case study) - Christian Borchert **moebel.de**
+
+**Social commerce** and the next level of direct-to-consumer brands - Tim Bibow **Stilnest**
+
+Making families happy - **taking the pain out of paying** - Thomas Ficht **MyToys**
+
+<img src="http://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1523564269/welcome1.jpg" alt="miłe twarze cudownych Uczestników" style="margin: 10px auto;" />
+
+Understanding Data Science: the basis of a **successful AI and Machine Learning strategy** - Nuno Batista **Swatch Group**
+
+**Amazon Advertising Best Practices** Learned from 500+ Clients - Nich Weinheimer **Kenshoo**
+
+eCommerce and **US Sales Tax** – why companies are automating - Sacha Wilson **Avalara**
+
+The Omnichannel Expectation: Unifying every **customer conversation** - Jonathan Anguelov **Aircall**
+
+**Optimizing for Search Bots** - Fili Wiese **SearchBrothers.com**
+
+**Human Intuition Vs Marketing Data** - Sharon Magen **DataWit**
+
+**Distributed Commerce Strategy** - Albert Vellve **Zalando**, Amr El-Shimi **Zalando**
+
+A Data-Driven Future - Bruno Gorgulho **Exponea**
+
+50% by 2020: Preparing for the **Future with Voice Search** - Dustin Coates **Algolia**
+
+[Panel] Customer engagement in the omnichannel world - [Moderator] Matej Kaposvary **Shopsys**, Safil Andersen **Celonis**, Markus Wuebben **CrossEngage**, Christopher Grohmann **Reputation.com**
+
+Starting up E-Commerce at TRUMPF - Rainer Schüßler **TRUMPF**
+
+Site search: the hidden gem of **conversion optimization** - Kylie Fletcher **Sooqr**
+
+How AVON's **3-step on-site messaging strategy** boosted its sales by 150% - Judit Pal **OptiMonk**
+
+Custom packaging & **unboxing experience.** - Jure Siric **Modepack**
+
+**Cross-border expansion** for more e-sales - the game, the players, the success. - Rolando Casanova **Internel**
+
+# Register here [https://ecommerceberlin.com/visit](https://ecommerceberlin.com/visit)
+
+How to prepare your webshop for **International expansion** - Aleah Raquel Belluga **Mollie**
+
+[Case Study] How Marley Spoon increases brand awareness and purchases with an **innovative campaign on Youtube**? - Shefali Bhatnagar **Google**, Andri Fried **Marley Spoon**
+
+How and why to go **multichannel** – A best practice case - Anastasia Hansen **Mister Spex**
+
+How to use Brand and Performance Marketing in Retail? [Case study] - Thomas Kloubert **Kfzteile24.de**
+
+Product strategy when **scaling a company** - Shimon Koifman **reBuy**
+
+The Pain-Points of Fashion P2P processes, and how to overcome them. - Claes Larsson **Zalando**
+
+How to Succeed Internationally with new emerging Marketing Channels: Social Media, **Google Actions**, Mobile Apps - Nenad Cetkovic **Lengow**
+
+# Register here [https://ecommerceberlin.com/visit](https://ecommerceberlin.com/visit)
+
+Connecting with Customers in the **Age of Assistance** - Mathias Blüm **Google**
+
+Mybauer.de & idealo: From the traditional shop in rural idyll to a big online player at idealo - Max Mayer **MyBauer.de**, Thomas Kurschat **idealo**
+
+The end of passwords: **Two-factor-authentication and biometrics** are coming 2019 - Carsten Mürl **Master Card**
+
+Enabling e-commerce mass customization business models - Christian Möhring **helmade**
+
+Create Your Own Payment Ecosystem - Delivering a Unified Payment Experience Across Multiple Businesses - Jan-Patrick Mainka **Optile**
+
+How AXA increased **ROI on affiliates** by 180% - Pavel Sima **Roivenue**
+
+# Register here [https://ecommerceberlin.com/visit](https://ecommerceberlin.com/visit)
+
+@endcomponent
+
+
+
