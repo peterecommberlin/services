201915fa12f6304e5eccf9574556b0f083ff7580
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-02-19 13:01:46 +0100

    up

--- resources/views/emails/visitor/ebe-ticket-final-reminder.blade.php
+++ resources/views/emails/visitor/ebe-ticket-final-reminder.blade.php
@@ -6,7 +6,7 @@
 
 You have recently registered for E-commerce Berlin Expo 2019!
 
-We hope to see you TOMORROW (Wednesday; 20th of February) at STATION Berlin.
+We hope to see you **TOMORROW (Wednesday; 20th of February) at STATION Berlin. Luckenwalder Str. 4-6**
 
 Have you downloaded your ticket?
 
@@ -16,7 +16,7 @@ Use the button below to download.
 Download ticket
 @endcomponent
 
-If you cannot print it - don't worry! We will print tickets at the reception desk.
+If you **cannot print it - don't worry!** We will print tickets at the reception desk.
 
 This time we will occupy to **two expo halls** to bring unprecedented quality of listening to the **36 presentations** and learning an offer of **almost 150 exhibitors.**
 
