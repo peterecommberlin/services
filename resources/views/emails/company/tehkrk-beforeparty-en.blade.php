49442019645f6548285aec98d33c642443e6d775
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-04-11 14:19:18 +0200

    edrone before party email

---
+++ resources/views/emails/company/tehkrk-beforeparty-en.blade.php
@@ -0,0 +1,32 @@
+
+
+@component('mail::message')
+ 
+# Hi {{$profile->translate("[[fname]]")}},
+
+eDrone - one of the exhibitors and Stage C Patron invites you to participate in the pre-event at their headquarters!
+
+<a href="https://www.facebook.com/events/411251339692029/"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1554984475/55929421_2365636603468365_9149853325007519744_o.jpg" alt="zaproszenie od eDrone" style="margin: 10px auto;" /></a>
+
+@component('mail::panel')
+
+**Start: 7pm** 16th of April - day before the Expo
+
+Location: **ul. Lekarska 1, Kraków**
+
+@endcomponent
+
+In the program, not only jazz, beer and networking but also a chance to listen to some interesting guests - including Marek Zmysłowski , who will talk about doing business in Africa "the fun way".
+
+If you are planning to choose, confirm the facebook event. [https://www.facebook.com/events/411251339692029/](https://www.facebook.com/events/411251339692029/) 
+
+If the duties on the expo hall let us - we will try to appear :)
+
+Regards, 
+
+Karolina Michalak
+
+Jakub Przybylski
+
+@endcomponent
+
---
+++ resources/views/emails/company/tehkrk-beforeparty-pl.blade.php
@@ -0,0 +1,35 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{$profile->translate("[[fname]]")}},
+
+
+eDrone czyli jeden z Wystawców i Mecenas Sceny C organizuje before party w swojej siedzibie w Krakowie.
+
+
+<a href="https://www.facebook.com/events/411251339692029/"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1554984475/55929421_2365636603468365_9149853325007519744_o.jpg" alt="zaproszenie od eDrone" style="margin: 10px auto;" /></a>
+
+@component('mail::panel')
+
+**Start: 19:00** (16.04.2019 - dzień przed dniem targowym)
+
+**ul. Lekarska 1, Kraków**
+
+@endcomponent
+
+
+W programie nie tylko jazz, piwo i networking ale też szansa na wysłuchanie kilku ciekawych Gości - m.in. Marka Zmysłowskiego, który opowie o biznesie w Afryce.
+
+Zapisy i pełna agenda via [https://www.facebook.com/events/411251339692029/](https://www.facebook.com/events/411251339692029/) 
+
+O ile obowiązki na hali nam pozwolą postaramy się również pojawić :)
+
+Pozdrawiamy, 
+
+Karolina Michalak
+
+Jakub Przybylski
+
+@endcomponent
+
