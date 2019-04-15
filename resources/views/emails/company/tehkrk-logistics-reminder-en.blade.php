929565a3cb45b964b77b2100a5b5ece8ee6dbc94
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-04-15 14:22:37 +0200

    logistics reminder

---
+++ resources/views/emails/company/tehkrk-logistics-reminder-en.blade.php
@@ -0,0 +1,70 @@
+
+
+@component('mail::message')
+ 
+# Hello!
+
+# Here you have basic logistic information about Ecommerce Cracow Expo:
+
+## Place:
+
+Targi w Krakowie
+
+ul. Galicyjska 9, 31-586 Kraków
+
+## (Optional) Setup day - 16th of April
+
+Assembly hours 4pm-8pm - **parking** will be open (free of charge) during that time. 
+
+After entering the hall you need to go to the **information point for the exhibitors**. Your Account Manager will happily await :)
+
+In case of any problem (e.g., with furniture, electricity) you should go to the information for exhibitors or call your Account Manager, whose number you will find in the welcome pack.
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1555329781/tehkrk_rozladunek.jpg" style="max-width: 600px; margin: 20px 0;" alt="" />
+
+("ROZŁADUNEK" on the map means recommended unloading zone for vehicles)
+
+## Event Day - 17th of April
+
+In the standard offering (without buying additional one) you have a **one parking card**, which is only needed to leave the parking area during event day.
+
+Additional setup hours are between **8:00am and 9:30pm.**
+
+After entering the hall you need to go to the information point for the exhibitors. Your Account Manager (or team member in charge) should be there.
+
+At 10:00am we will open gates and welcome visitors.
+
+In case of any problem (e.g., with electricity) you should go to the information for exhibitors or text your Account Manager, whose number you will find in the welcome pack.
+
+**Disassembly** is possible from 5:15pm till 8:00pm.
+
+
+## Welcome pack
+
+During our first meeting (optional setup day or immediately before the event) you will be handed a welcome pack with: 
+
+* the badges of your representatives if you have added them before the deadline at your exhibitor's panel, 
+* lunch vouchers, 
+* contact numbers to the important staff of Expo, 
+* parking card
+* any additionally paid vouchers, parking cards...
+
+
+## Additional recommendations
+
+** Please take your own power extension cord / bar ** (of course we have some backup but...)
+
+** Remember, that standard power access is only 300 Watts ** 
+
+If you plan to use something more that, for instance, 2 laptops and TV you need to buy an extra package (3000 Watts).  If you want to consult power usage of your equipment, please contact with us.
+
+
+
+Regards, 
+
+Karolina Michalak,
+
+Jakub Przybylski
+
+@endcomponent
+
---
+++ resources/views/emails/company/tehkrk-logistics-reminder-pl.blade.php
@@ -0,0 +1,68 @@
+
+
+@component('mail::message')
+ 
+# Cześć
+
+# Oto podstawowe informacje logistyczne:
+
+## Miejsce Targów
+
+Targi w Krakowie, ul. Galicyjska 9, 31-586 Kraków
+
+## Dzień montażowy (opcjonalny) - 16 kwietnia
+
+**Parking podczas dnia montażowego jest otwarty przez cały dzień.**
+
+Godziny montażowe to **16:00 - 20:00** (o ile nie ustalono z nami innych).
+
+Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców na którym będzie czekał Państwa Opiekun, który zaprowadzi Państwa do właściwego stoiska.
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1555329781/tehkrk_rozladunek.jpg" style="max-width: 600px; margin: 20px 0;" alt="" />
+
+## Dzień Targowy - 17 kwietnia
+
+Montaż w dzień targowy możliwy jest tylko i wyłącznie w godzinach **8:00 - 9:30.** (O 10:00 hala zostanie otwarta do publicznego zwiedzania.)
+
+Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców na którym będzie czekał Państwa Opiekun, który zaprowadzi Państwa do właściwego stoiska.
+
+O ile nie została dokupiona dodatkowa, standardowo mają Państwo do dyspozycji jedną kartę parkingową, która jest potrzebna **tylko do wyjazdu z parkingu.**
+
+Karta parkingowa będzie znajdowała się w "welcome packu" na stoisku
+
+**Demontaż** jest możliwy od 17:15. Hala będzie dostępna do 20:00.
+
+W razie wystąpienia jakichkolwiek problemów i wątpliwości związanych z Państwa stoiskiem (np z prądem) należy udać się do informacji dla wystawców lub dzwonić na numer do Opiekuna, który znajduje się w kopercie / pakiecie powitalnym.
+
+
+## Welcome pack
+
+Welcome pack zostanie przekazany podczas naszego pierwszego spotkania (16 kwietnia lub w dzień targów). 
+
+* identyfikatory przedstawicieli (jeśli zostali dodani w Panelu Wystawcy :), 
+* vouchery obiadowe, 
+* numery kontaktowe do osób z obsługi targów, 
+* karta parkingowa.
+
+
+## Dodatkowe informacje i zalecenia
+
+** Prosimy o wzięcie własnych listew zasilających **
+
+** Prosimy pamiętać, że standardowe przyłącze prądu to tylko 300W** jeśli planujesz coś więcej niż korzystanie z 2 laptopów i ew. TV trzeba dokupić indywidualne przyłącze. Prosimy o kontakt jeśli masz wątpliwości.
+
+@component('mail::panel')
+
+UWAGA: W przypadku podklejania wykładzin we własnym zakresie (brak wykupienia stosownej usługi od nas) jedyną rekomendowaną opcją jest profesjonalna taśma dwustronna ETG firmy Imawex. Typowa (=marketowa) taśma dwustronna jest nie do usunięcia po wydarzeniu co wygeneruje dodatkowe (kosmiczne) koszty sprzątania.
+
+@endcomponent
+
+
+Regards, 
+
+Karolina Michalak,
+
+Jakub Przybylski
+
+@endcomponent
+
