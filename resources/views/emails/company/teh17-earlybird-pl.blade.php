4bb9b6a290380c257f30c009c478f88a2e339852
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-05-15 14:32:23 +0200

    earlybird

---
+++ resources/views/emails/company/teh17-earlybird-pl.blade.php
@@ -0,0 +1,34 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{$profile->translate("[[fname]]")}},
+
+Witamy miesiąc po Targach. Tym razem ankieta podsumowująca trochę później gdyż interesują nas biznesowe korzyści / konwersja z minionych Targów.
+
+@component('mail::button', ['url' => "https://forms.gle/xZeqRPNGX8P3qRic8"])
+Otwórz ankietę i wypełnij
+@endcomponent
+
+**Po wypełnieniu ankiety otrzymasz - od razu - dostęp do galerii zdjęć HI-RES do dowolnego wykorzystania przez Twoją firmę**
+
+# Sprzedaż stoisk na kolejne Targi (22 października w Warszawie)
+
+We wtorek **21 maja** o godzinie **12:00** startuje sprzedaż Early Bird dla Wystawców minionych Targów eHandlu w Krakowie, którzy nie znaleźli się w rankingu TOP30 Konkursu dla Wystawców.
+
+Pula early bird będzie limitowana (ok 20 stoisk) ale po jej zakończeniu ruszy I regularna pula stoisk dla wszystkich zainteresowanych.
+
+**Przypomnienie oraz adres strony internetowej do rezerwacji będą wysłane w poniedziałek ok 12:00**
+
+@component('mail::button', ['url' => "https://d16rm1n165bd05.cloudfront.net/items/0c1A0w2M0W0B1N3d010N/Image%202019-05-15%20at%201.43.45%20PM.png"])
+Zobacz układ hali targowej
+@endcomponent
+
+Gorąco pozdrawiamy,
+
+Karolina Michalak, Jakub Przybylski
+
+@endcomponent
+
+
+
