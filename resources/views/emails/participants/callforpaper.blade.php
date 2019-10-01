cba02d27f12d5eb1a8cf0106a47375338997734b
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-10-01 14:14:54 +0200

    cfp msg update

--- resources/views/emails/participants/callforpaper.blade.php
+++ resources/views/emails/participants/callforpaper.blade.php
@@ -4,13 +4,15 @@
  
 # Cześć {{$profile->translate("[[fname]]")}},
 
-Bardzo dziękujemy za udział w walce o udział w Agendzie XVII Targów eHandlu.
+Bardzo dziękujemy za udział w walce o miejsce w Agendzie XVII Targów eHandlu.
 
 ## Twoja ostateczna liczba głosów to: {{$votes}}
 
-Z firmami, które wygenerowały ~ 100 punktów skontaktujemy się dzisiaj celem wyboru sceny prezentacyjnej.
+Najwyższą liczbę głosów (127) zdobył Paweł Regiec z eStrategie.pl (gratulacje!). Na wszystkie zgłoszenia oddano ponad 2000 głosów.
 
-Z wybranymi (analiza merytoryczna zgłoszenia) - pozostałymi - uczestnikami Konkursu będziemy kontaktować się do końca tego tygodnia.
+**Z firmami, które wygenerowały ~ 100 punktów skontaktujemy się dzisiaj celem wyboru sceny prezentacyjnej.**
+
+**Z wybranymi (analiza merytoryczna zgłoszenia) - pozostałymi - uczestnikami Konkursu będziemy kontaktować się do końca tego tygodnia.**
 
 Regards, 
 
