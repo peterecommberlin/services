4bc71bc146e691b5f5fab4fa354ab5ef46afa012
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-10-03 14:25:55 +0200

    upgrades2

---
+++ resources/views/emails/company/teh17-upgrades2-en.blade.php
@@ -0,0 +1,48 @@
+
+
+@component('mail::message')
+ 
+# Hello,
+
+**We kindly remind you that in your exhibitor's account admin panel you can now order additional services.**
+
+If you are interested in any of the services below, **please order them before the weekend** - it will cause much help to our work (processing, invoicing, etc..)
+
+List of available options is as follows:
+
+Additional **catering voucher(s)** (maximum 4 vouchers will be included)
+
+Additional **parking card(s)** (1 parking card will be included)
+
+**50 inch LED display (HDMI + USB) on the stand**
+
+Permission to distribute **leaflets outside booth** (for instance: a hostess is welcoming visitors near the entrance or in networking zones)
+
+Additional **electricity** connection - **3000 Watts** (instead 300 W) if you plan to bring something more than 1-2 laptops to your booth
+
+and booth arrangement related services: 
+
+Complete **booth arrangement** with fullsize fullcolor print, counter, leaflet holder
+
+same as above but with OSB back wall and logotype
+
+**counter with branding**
+
+**flooring carpet**
+
+additional **high chair(s)** - 2 are already included
+
+additional **table(s)** - 1 is already included
+
+## We need to emphasize that **some services are limited by quantity and order date**. For instance the booth arrangement options must be ordered till October 9th.
+
+Check out prices, images, specs by clicking the button below. 
+
+@component('mail::button', ['url' => $accountUrl])
+Sign Into My Account
+@endcomponent
+
+Regards! Karolina Michalak
+
+@endcomponent
+
---
+++ resources/views/emails/company/teh17-upgrades2-pl.blade.php
@@ -0,0 +1,41 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{$profile->translate("[[fname]]")}},
+
+**Przypominamy, że w panelu Wystawcy możesz rozszerzyć swoje zamówienie o dodatkowe usługi.**
+
+Jeśli planujesz dokupienie czegoś z poniższej listy - prosimy o dokonanie wyboru przed weekendem. **Bardzo ułatni nam to pracę...**
+
+Lista aktualnych usług to:
+
+* Dodatkowe vouchery cateringowe (poza maksymalnie 4, które są w cenie stoiska)
+* Dodatkowa karty parkingowe (poza 1, która jest w cenie)
+* Ekran LED 50 cali ze statywem
+* Prawo do dystrybucji ulotek poza stoiskiem (np przy wejściu i w 2 strefach networkingu)
+* Niezależne podłączenie prądu 3000W (zamiast standardowych 300W) jeśli planujesz wyposażenie stoiska inne niż 1-2 laptopy i TV
+
+
+oraz 
+
+* Kompleksowa zabudowa przestrzeni stoiska w wersji FULLPRINT
+* Kompleksowa zabudowa przestrzeni stoiska w wersji OSB+logotyp
+* Lada z nadrukiem
+* Wykładzina
+* Dodatkowe meble
+
+Uwaga: Niektóre świadczenia są **ograniczone ilościowo** jak i **czasowo** (np zamówienie związane z zabudową tylko do 9 października)
+
+Sprawdź szczegóły korzystając z przycisku poniżej (przejdź na podstronę "Usługi dodatkowe")
+
+@component('mail::button', ['url' => $accountUrl])
+Zaloguj się do Panelu Wystawcy 
+@endcomponent
+
+Pozdrawiam, Karolina Michalak
+
+@endcomponent
+
+
+
