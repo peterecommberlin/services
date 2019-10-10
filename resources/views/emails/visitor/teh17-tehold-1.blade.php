9374bd032a8bacbf8f4c326e28296eafe57e29eb
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-10-10 15:59:37 +0200

    fix

--- resources/views/emails/visitor/teh17-tehold-1.blade.php
+++ resources/views/emails/visitor/teh17-tehold-1.blade.php
@@ -4,17 +4,22 @@
 
 Reklamy **Google i Facebook Ads** w 2020, **Handel międzynarodowy** (np z wykorzystaniem rozwiązań jak **Shopware**, **Stripe** czy platform jak **Amazon**), wyższość **progressive web apps** nad aplikacjami mobilnymi, efekty rewolucji związane z wejściem dyrektywy **PSD2**, strategie **omnichannel**, trendy w **opakowaniach** to tylko część tego co będzie się działo na **4 scenach XVII Targów eHandlu.**
 
+@component('mail::button', ['url' => $registerURl])
+	Odbierz bilet na Prezentacje i EXPO za 0 PLN
+@endcomponent
+
 #22 października w Warszawie (na terenie EXPO XXI) odbędą się największe targi przeznaczone dla sprzedających w Internecie w tej części Europy. 
 
-@component('mail::button', ['url' => $registerURl])
-	 Bilet na 34 prezentacje za 0 PLN
-@endcomponent
 
 34 Prelegentów, ponad 130 Wystawców (w tym wielu po raz pierwszy w Polsce!), aż 2 strefy networkingowe (dzięki wsparciu Google Internetowe Rewolucje oraz DPD)
 
 <a href="{{$siteUrl}}"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1570713010/esklepy.png" alt="przykładowe prezentacje eSklepów" style="margin: 10px auto;" /></a>
 
 Wśród prelegentów przedstawiciele takich sklepów internetowych jak **Inter Cars, Pomadziarz, Agencja Mienia Wojskowego czy szkla.com.** **Poznaj składniki ich sukcesu** i wykorzystaj w zwiększeniu świadomości marki i poziomu sprzedaży.
+
+@component('mail::button', ['url' => $registerURl])
+	 Bilet na 34 prezentacje za 0 PLN
+@endcomponent
 
 Sukces sklepów internetowych to najczęściej tytaniczna wręcz praca ich założycieli czy osób zarządzających. W świecie szybko ewoluujących rozwiązań wspomagających komunikację i sprzedaż online ciężko jednak obejść się bez wiedzy konsultantów i firm, których istnienie silnie jest związane z sukcesem ich klientów. Na Targach eHandlu znajdziesz także wiedzę z tej strony "barykady". 
 
