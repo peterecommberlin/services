350986891effb33b4656b49dda9fa9243f9a1fee
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-10-11 14:20:03 +0200

    en logistics

---
+++ resources/views/emails/company/teh17-logistics-en.blade.php
@@ -0,0 +1,85 @@
+
+
+@component('mail::message')
+ 
+# Hello!
+
+# Here you have basic logistic information about E-commerce Warsaw Expo:
+
+## Oversized parcels and materials which you want to ship and have delivered directly to the booth. Recipient:
+
+@component('mail::panel')
+
+ExpoSped Sp. z o.o. / Piotr Kopański
+
+EHANDEL [booth ID / or BRAND name]
+
+ul. Prądzyńskiego 12/14 (street name)
+ 
+01-222 Warszawa 
+
+(contact person: Piotr Kopański mobile +48504103570)
+
+@endcomponent
+
+
+## Place:
+
+EXPO XXI (HALL #1)
+
+ul. Prądzyńskiego 12/14
+
+01-222 Warszawa 
+
+
+## (Optional) Setup day - 21st of October
+
+In the standard offering (without buying additional one) you have a **one parking card**, which is only needed to leave the parking area. Wwhen entering the parking from the street you always take the receipt as normal guest)
+
+Assembly hours 4pm-8pm
+
+After entering the main entrance you need to go to the **information point for the exhibitors** (located in the lobby or Hall 1) . Your Account Manager will happily await :)
+
+In case of any problem (e.g., lack of furniture, electricity) you should go to the information for exhibitors or call your Account Manager, whose number you will find in the welcome pack. 
+
+## Event Day - 22nd of October
+
+In the standard offering (without buying additional one) you have a **one parking card**, which is only needed to leave the parking area. When entering the parking from the street you always take the receipt as normal guest.
+
+Additional setup hours are between **8:00am and 9:30pm.** If you have your booth built by us this is sufficient moment to show at the Expo.
+
+After entering the hall you need to go to the information point for the exhibitors (100% located in the Hall 1 / Vip Area) . Your Account Manager (or team member in charge) should be there.
+
+At 10:00am we will open gates and welcome visitors.
+
+In case of any problem (e.g., with electricity) you should go to the information for exhibitors or text your Account Manager, whose number you will find in the welcome pack.
+
+**Disassembly** is possible from 5:15pm till 8:00pm.
+
+## Welcome pack
+
+During our first meeting (optional setup day or immediately before the event) you will be handed a welcome pack with: 
+
+* the badges of your representatives if you have added them before the deadline at your exhibitor's panel, 
+* lunch vouchers (number of vouchers = number of representatives registered but max 4... need more? please buy addditional via Additional services in your Exhibitor's Panel)
+* contact numbers to the important staff of Expo, 
+* parking card
+* any additionally paid vouchers, parking cards...
+
+## Additional recommendations
+
+** Please take your own power extension cord / bar ** (of course we have some backup but...)
+
+** Remember, that standard power access is only 300 Watts ** 
+
+If you plan to use something more that, for instance, 2 laptops and TV you need to buy an extra package (3000 Watts).  If you want to consult power usage of your equipment, please contact with us.
+
+
+Regards, 
+
+Karolina Michalak,
+
+Jakub Przybylski
+
+@endcomponent
+
