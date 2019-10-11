6eaaff1342482d6fecaadf1cd6c14a3163db1908
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-10-11 11:20:55 +0200

    info logistyczne

---
+++ resources/views/emails/company/teh17-logistics-pl.blade.php
@@ -0,0 +1,80 @@
+
+
+@component('mail::message')
+ 
+# Cześć
+
+# Oto podstawowe informacje logistyczne:
+
+## Wysyłka paczek i materiałów, które mają trafić bezpośrednio na Targi.
+
+@component('mail::panel')
+
+ExpoSped Sp. z o.o. / Piotr Kopański
+EHANDEL [nr stoiska lub marka handlowa firmy]
+ul. Prądzyńskiego 12/14
+01-222 Warszawa
+(tel. kontaktowy do przedstawiciela ExpoSped: Piotr Kopański +48 504103570)
+
+@endcomponent
+
+**Wysłane paczki znajdą się na Państwa stoisku podczas dnia montażowego czyli 21 października (w poniedziałek). Hala jest zamykana i ochraniana po wyjściu organizatora w dzień montażowy.**
+
+## Miejsce Targów
+
+Expo XXI, ul. Prądzyńskiego 12/14, 01-222 Warszawa
+
+*ta edycja będzie realizowana w HALI 1 (największej)* 
+
+## Parkowanie (montaż, dzień eventu)
+
+Bilet parkingowy należy pobrać z parkomatu. **Do bezpłatnego wyjazdu** potrzebny będzie voucher, który dostaniecie Państwo od nas (w dzień montażowy) lub znajdziecie Państwo w welcome packu na stoisku.
+
+## Dzień montażowy (opcjonalny) - 21 października
+
+Godziny montażowe to 16:00 - 20:00 (o ile nie ustalono z nami inaczej).
+
+Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców na którym będzie czekał Państwa Opiekun, który zaprowadzi Państwa do właściwego stoiska.
+
+**Punkt będzie znajdował się w Hali #1**
+
+## Dzień Targowy - 22 października
+
+Montaż w dzień targowy możliwy jest tylko i wyłącznie w godzinach **8:00 - 9:30.** (O 10:00 hala zostanie otwarta do publicznego zwiedzania.)
+
+Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców na którym będzie czekał Państwa Opiekun, który zaprowadzi Państwa do właściwego stoiska.
+
+**Demontaż** jest możliwy od 17:15. Hala będzie dostępna do 20:00 (o ile nie ustalono z nami inaczej).
+
+W razie wystąpienia jakichkolwiek problemów i wątpliwości związanych z Państwa stoiskiem (np z prądem) należy udać się do informacji dla wystawców lub dzwonić na numer do Opiekuna, który znajdzie się w kopercie / pakiecie powitalnym.
+
+## Welcome pack
+
+Welcome pack zostanie przekazany podczas naszego pierwszego spotkania (21 października lub w dzień targowy 22 października). 
+
+* identyfikatory przedstawicieli (jeśli zostali dodani w Panelu Wystawcy :), 
+* vouchery obiadowe (w liczbie zarejestrowanych w Panelu Wystawcy ale nie więcej niż 4 sztuki - chyba, że dokupiono dodatkowe...)
+* numery kontaktowe do osób z obsługi targów, 
+* karta parkingowa.
+
+
+## Dodatkowe informacje i zalecenia
+
+** Prosimy o wzięcie własnych listew zasilających **
+
+** Prosimy pamiętać, że standardowe przyłącze prądu to tylko 300W** jeśli planujesz coś więcej niż korzystanie z 2 laptopów i ew. TV trzeba dokupić indywidualne przyłącze (Panel Wystawcy). Prosimy o kontakt jeśli masz wątpliwości.
+
+@component('mail::panel')
+
+UWAGA: W przypadku podklejania wykładzin we własnym zakresie (brak wykupienia stosownej usługi od nas) jedyną rekomendowaną opcją jest profesjonalna taśma dwustronna ETG firmy Imawex. Typowa (=marketowa) taśma dwustronna jest nie do usunięcia po wydarzeniu co wygeneruje dodatkowe (kosmiczne) koszty sprzątania, które będziemy musieli refakturować.
+
+@endcomponent
+
+Regards, 
+
+Karolina Michalak,
+
+Jakub Przybylski
+
+@endcomponent
+
