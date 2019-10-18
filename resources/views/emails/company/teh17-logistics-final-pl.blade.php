0216247eb8d198feade40c13859db8c55a5a63bb
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-10-18 13:37:12 +0200

    logistic final

---
+++ resources/views/emails/company/teh17-logistics-final-pl.blade.php
@@ -0,0 +1,70 @@
+
+
+@component('mail::message')
+ 
+# Cześć
+
+Przypominamy kluczowe informacje logistyczne.
+
+Rozładunek samochodów będzie możliwy korzystając z 2 bram zaznaczonych na grafice poniżej.
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1571398393/teh17_bramy.png" alt="bramy wjazdowe" style="margin: 10px auto;" />
+
+## Miejsce Targów
+
+Expo XXI, ul. Prądzyńskiego 12/14, 01-222 Warszawa
+
+*ta edycja będzie realizowana w HALI 1 (największej)* 
+
+## Parkowanie (montaż, dzień eventu)
+
+Bilet parkingowy należy pobrać z parkomatu. **Do bezpłatnego wyjazdu** potrzebny będzie voucher, który dostaniecie Państwo od nas (w dzień montażowy) lub znajdziecie Państwo w welcome packu na stoisku (w dzień eventu)
+
+## Dzień montażowy (opcjonalny) - 21 października
+
+Godziny montażowe to 16:00 - 20:00 (o ile nie ustalono z nami inaczej).
+
+Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców (Strefa VIP - początek Hali #1) na którym będzie czekał Państwa Opiekun, który zaprowadzi Państwa do właściwego stoiska.
+
+**Punkt będzie znajdował się w lobby zespołu budynków 1-2-3 lub w Hali #1**
+
+## Dzień Targowy - 22 października
+
+Montaż w dzień targowy możliwy jest tylko i wyłącznie w godzinach **8:00 - 9:30.** (O 10:00 hala zostanie otwarta do publicznego zwiedzania.)
+
+Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców na którym będzie czekał Państwa Opiekun, który zaprowadzi Państwa do właściwego stoiska.
+
+**Demontaż** jest możliwy od 17:15. Hala będzie dostępna do 20:00 (o ile nie ustalono z nami inaczej).
+
+W razie wystąpienia jakichkolwiek problemów i wątpliwości związanych z Państwa stoiskiem (np z prądem) należy udać się do informacji dla wystawców lub dzwonić na numer do Opiekuna, który znajdzie się w kopercie / pakiecie powitalnym.
+
+## Welcome pack
+
+Welcome pack zostanie przekazany podczas naszego pierwszego spotkania (21 października lub w dzień targowy 22 października). 
+
+* identyfikatory przedstawicieli (jeśli zostali dodani w Panelu Wystawcy :), 
+* vouchery obiadowe (w liczbie zarejestrowanych w Panelu Wystawcy ale nie więcej niż 4 sztuki - chyba, że dokupiono dodatkowe...)
+* numery kontaktowe do osób z obsługi targów, 
+* karta parkingowa.
+
+
+## Dodatkowe informacje i zalecenia
+
+** Prosimy o wzięcie własnych listew zasilających **
+
+** Prosimy pamiętać, że standardowe przyłącze prądu to tylko 300W** jeśli planujesz coś więcej niż korzystanie z 2 laptopów i ew. TV trzeba dokupić indywidualne przyłącze (Panel Wystawcy). Prosimy o kontakt jeśli masz wątpliwości.
+
+@component('mail::panel')
+
+UWAGA: W przypadku podklejania wykładzin we własnym zakresie (brak wykupienia stosownej usługi od nas) jedyną rekomendowaną opcją jest profesjonalna taśma dwustronna ETG firmy Imawex. Typowa (=marketowa) taśma dwustronna jest nie do usunięcia po wydarzeniu co wygeneruje dodatkowe (kosmiczne) koszty sprzątania, które będziemy musieli refakturować.
+
+@endcomponent
+
+Regards, 
+
+Karolina Michalak,
+
+Jakub Przybylski
+
+@endcomponent
+
