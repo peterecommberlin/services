3c06a9735a27fb45eab24d21b8e69a32c293e5c6
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-10-21 11:44:49 +0200

    visitor final invitation

---
+++ resources/views/emails/visitor/teh17-tehold-final.blade.php
@@ -0,0 +1,82 @@
+
+
+@component('mail::message')
+
+Mocny news na dzień przed warszawskimi Targami eHandlu!
+
+# Eksperci z **Google Internetowe Rewolucje** z warsztatami i bezpłatnymi konsultacjami!
+
+Lider zespołu doradców Paweł Bilczyński przeprowadzi **45 minutowy  warsztat** na temat rozwijania biznesu globalnie przy wykorzystaniu narzędzi Google (np. Google Market Finder). Z warsztatu dowiesz się też w jaki sposób ocenić potencjał rynków zagranicznych.
+
+<a href="https://targiehandlu.pl/undefined-undefined,s,98061"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1571648433/mailing.png" alt="szef doradców" style="margin: 10px auto;" /></a>
+
+@component('mail::button', ['url' => $registerURl])
+	Bilet na wszystko za 0pln? Proszę!
+@endcomponent
+
+## **Przez 7 godzin** trwania Targów zespół doradców Google będzie udzielał **konsultacji na specjalnej strefie Google.**
+
+<a href="https://targiehandlu.pl/google-internetowe-rewolucje,c,1541"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1571612305/strefa_google.png" alt="jak wygląda strefa google internetowe rewolucje" style="margin: 10px auto;" /></a>
+
+Jeśli sprzedaż międzynarodowa to jeszcze nie etap ekspansji Twojego eSklepu nie martw się. **34 prelekcje** Targów stanowią przekrój przez wszystko co aktualnie istotne - **generowanie sprzedaży, finansowie, PWA, FB Ads, Google Ads, mobile commerce**.
+
+# Sprawdź kompletną agendę **4 scen** prezentacyjnych 17 Targów eHandlu w Warszawie!
+
+<a href="{{$scheduleURl}}"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1571146630/teh17_schedule.png" alt="lista prezentacji" style="margin: 10px auto;" /></a>
+
+## Przypominamy, że udział w prezentacjach i zwiedzaniu 140 stoisk nie wiąże się z żadnymi kosztami - jedynym warunkiem jest rejestracja online.
+
+@component('mail::button', ['url' => $scheduleURl])
+	Agenda i Rejestracja Online
+@endcomponent
+
+Reklamy **Google i Facebook Ads** w 2020, **Handel międzynarodowy** (np z wykorzystaniem rozwiązań jak **Shopware**, **Stripe** czy platform jak **Amazon**), wyższość **progressive web apps** nad aplikacjami mobilnymi, efekty rewolucji związane z wejściem dyrektywy **PSD2**, strategie **omnichannel**, trendy w **opakowaniach** to tylko część tego co będzie się działo na **4 scenach XVII Targów eHandlu.**
+
+
+<a href="{{$siteUrl}}"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1570713010/esklepy.png" alt="przykładowe prezentacje eSklepów" style="margin: 10px auto;" /></a>
+
+Wśród prelegentów przedstawiciele takich sklepów internetowych jak **Inter Cars, Pomadziarz, Agencja Mienia Wojskowego czy szkla.com.** **Poznaj składniki ich sukcesu** i wykorzystaj w zwiększeniu świadomości marki i poziomu sprzedaży.
+
+@component('mail::button', ['url' => $registerUrl])
+	Agenda i Rejestracja Online
+@endcomponent
+
+Sukces sklepów internetowych to najczęściej tytaniczna wręcz praca ich założycieli czy osób zarządzających. W świecie szybko ewoluujących rozwiązań wspomagających komunikację i sprzedaż online ciężko jednak obejść się bez wiedzy konsultantów i firm, których istnienie silnie jest związane z sukcesem ich klientów. Na Targach eHandlu znajdziesz także wiedzę z tej strony "barykady". 
+
+<a href="{{$registerURl}}"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1570713010/agencje.png" style="margin: 10px auto;" alt="" /></a>
+
+Oto lista tematów wystąpień, które szczególnie mogą Cię zainteresować.
+
+**Zdjęcie do ecommerce za mniej niż 1 PLN**
+
+> Jak robić setki zdjęć produktów dziennie, zachowując wysoką jakość i swój unikalny styl? Czy zdjęcia sprzedają? Czy twój biznes potrzebuje unikalnych zdjęć? Ilu? Jakiej jakości? Chcesz robić je samodzielnie czy zlecać profesjonalistom? I w zasadzie ile to wszystko kosztuje? Automatyzacja tworzenia zdjęć produktowych dla ecommerce i nie tylko.
+
+**Opisy kategorii wspierające SEO - czy naprawdę muszą mieć kilka tysięcy znaków?**
+
+> W temacie tekstów tworzonych z myślą o SEO krąży wiele mitów, które skutkują powstawaniem nieużytecznych, zbyt obszernych opisów na stronach kategorii. Podczas prezentacji omówię sprawdzone taktyki planowania i tworzenia treści tekstowych, które wspierają pozycjonowanie, a jednocześnie nie zaburzają użyteczności. Poruszę kwestię analizy słów kluczowych uwzględniającej świeże podejście do preferencji algorytmu Google, a także samego planowania układu strony kategorii.	
+
+**Proste błędy, które rujnują wyniki Twoich kampanii na Facebooku**
+
+W przypadku reklam na Facebooku mały zmiany przynoszą duże zwroty i decydują o powodzeniu kampanii. W swoim wystąpieniu przedstawię 5 najczęściej popełnianych błędów przy tworzeniu reklam na FB i opowiem, jak ich uniknąć.	
+
+@component('mail::button', ['url' => $scheduleURl])
+	Agenda i Rejestracja Online
+@endcomponent
+
+Targi eHandlu to także Targi! Przez 7 godzin zwiedzania możesz poznać praktycznie całą, kluczową, panoramę rozwiązań wspierających skuteczną sprzedaż online :) 
+
+**Widzimy się w Warszawie? :)**
+
+Do zobaczenia! 
+
+Katarzyna Wicher, Targi eHandlu
+
+## [Zarządzaj wysyłką]({{ $unsubscribe }}) 
+
+@endcomponent
+
+
+
+
+
+
