1d38800d432813179abaa0e550bc90e19a20de20
peterecommberlin <35766428+peterecommberlin@users.noreply.github.com>
2020-01-13 21:22:00 +0100

    first logistics info for 2020

---
+++ resources/views/emails/company/ebe5-logistics-manual-1st-message-en.blade.php
@@ -0,0 +1,91 @@
+
+
+@component('mail::message')
+ 
+# Hello {{$profile->translate("[[fname]]")}},
+
+# Here you have basic logistic information about E-commerce Berlin Expo:
+
+## Oversized parcels and materials which you want to ship and have delivered directly to the booth. Recipient:
+
+@component('mail::panel')
+
+STATION Berlin Luckenwalder Str 4-6 
+
+10963 Berlin
+
+Contact person: Nadine Geyer
+
+Please describe parcel as 'E-commerce Berlin 2020 Expo [number of the booth OR name of the company]'
+
+Parcel can reach the place no earlier than February, the 7th! All parcels will be available at two info points at the exhibition hall. You can collect them there. 
+
+@endcomponent
+
+## Place:
+
+STATION Berlin  - Luckenwalder Str. 4-6, 10963 Berlin 
+
+
+## Parking during assembly day (Wednesday / 12 Feb)
+
+You can park your car at the venue during the assembly day in front of the restrictive halls. 
+
+If your exhibition space is at **hall 1** (right floor on the map), you can park your car entering from **Luckenwalder Str.** 
+
+If your exhibition space is in **hall 2** (the left one on the map) you can park your car entering from **Trebbiner Str.**
+
+The venue owner will take a **deposit** for cars worth **50€** and for **trucks 100€**. The deposit will be given back when you leave the venue during the assembly day.
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/w_650,c_fit/v1549965815/images/SBWEB_Grundriss_1024x768_151102_Loft.png" alt="map" style="max-width: 650px; " />
+
+## Parking during EXPO day (Thursday / 20 Feb)
+
+Parking during the Expo day is **not possible on the area of the venue. **
+
+Next to the STATION Berlin, there is a multilevel parking house which can be used during the Expo day. 
+
+The one-day-ticket is 10€ worth. 
+
+## Assembly day 
+
+* 12th of February from 1:00 PM to Midnight
+* 13th of February from 8:00 AM to 9:30 AM.
+
+After entering the hall you need to go to one of the two **information points for the exhibitors** (one info point per hall). Your Account Manager will await :)
+
+There, you will get your exhibitor **welcoming package** which includes: 
+
+* the badges of your representatives if you have added them before the deadline at your exhibitor's panel,
+* lunch vouchers,
+* internet network access - login data will be included. 
+* contact numbers to the staff of Expo,
+
+
+** Remember, that standard power access is:**  a) only 500 Watts (for the categories Start-Up, Hot/Super Hot). b) The grand space has 1000 Watts. 
+
+If you plan to use something more and it needs more electricity, please contact us as soon as possible. If you want to consult power usage of your equipment, please contact us.
+
+## Expo day
+
+13th of February from 10.00 AM to 5.00 PM.
+
+In case of any problem (e.g., with electricity) you should go to the information for exhibitors or call your Account Manager, whose number you will find in the welcome pack.
+
+## Disassembly
+
+13th of February. Disassembly is possible from 5.15 PM to 10.00 PM. If you need to upload your car with your stuff, you will be able to drive on the venue from 6.00 PM, not earlier. 
+
+The same rules are valid as mentioned in the assembly day section. 
+
+## You can also download a pdf file with a package of logistics information for the exhibitors. Please click the link below. 
+
+[DOWNLOAD THE MANUAL FOR THE EXHIBITORS](https://res.cloudinary.com/ecommerceberlin/image/upload/v1578945741/Exhibitors/exhibitors%20manual%202020/ecommerce_berlin_exhibitors_guide_2020_.pdf)
+
+
+Regards, 
+
+E-commerce Berlin Expo Team
+
+@endcomponent
+
