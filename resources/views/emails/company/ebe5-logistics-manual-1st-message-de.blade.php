5bb58e304dfa936a5b26575afe283876281801a3
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-01-14 12:51:28 +0100

    EBE emails

--- resources/views/emails/company/ebe5-logistics-manual-1st-message-de.blade.php
+++ resources/views/emails/company/ebe5-logistics-manual-1st-message-de.blade.php
@@ -4,13 +4,13 @@
  
 # Hello {{$profile->translate("[[fname]]")}},
 
-# Hier findest du Infos rund um die Logistik für die anstehende E-commerce Berlin 2020 Expo. 
+Hier findest du Infos rund um die Logistik für die anstehende E-commerce Berlin 2020 Expo.
 
-# Vorab, Lieferungen von großen Gegenständen können zum Veranstaltungsort geschickt werden. Sie werden auch auf der Standfläche warten. Folgende Informationen müssen die Lieferungen enthalten: 
-
-@component('mail::panel')
+## Vorab, Lieferungen von großen Gegenständen können zum Veranstaltungsort geschickt werden. Sie werden auch auf der Standfläche warten. Folgende Informationen müssen die Lieferungen enthalten:
 
 Empfänger: 
+
+@component('mail::panel')
 
 STATION Berlin, Luckenwalder Str. 4-6, 10963 Berlin 
 
@@ -26,17 +26,18 @@ Kleinere Pakete werden bei den Infopunkten auf dem Messegelände warten und kön
 
 STATION Berlin, Luckenwalder Str. 4-6, 10963 Berlin 
 
-
-# Parksituation: 
+## Parksituation: 
 
 Das Parken auf dem Gelände der Expo ist am Tag der Expo nicht möglich. 
-Neben dem Expogelände gibt es ein Parkhaus, welches benutzt werden kann. Das Tagesticket kostet 10€. 
+
+Neben dem Expogelände gibt es **ein Parkhaus**, welches benutzt werden kann. **Das Tagesticket kostet 10€.**
 
 <img src="https://res.cloudinary.com/ecommerceberlin/image/upload/w_650,c_fit/v1549965815/images/SBWEB_Grundriss_1024x768_151102_Loft.png" alt="map" style="max-width: 650px; " />
 
 Ihr könnt am Aufbautag (12.02.2020) mit den Autos auf dem Gelände vor den Hallen parken.
 
 Wenn deine Ausstellungsfläche in der **ersten Halle** ist (rechts auf der Mappe), dann kannst du von der **Luckenwalder Straße** aus reinfahren und dort parken. 
+
 Wenn deine Ausstellungsfläche in der **zweiten Halle** ist (links auf der Mappe) dann kannst du von der **Trebbine Straße** aus reinfahren und dort parken. 
 
 Beim Reinfahren wird eine **Kaution iHv. 50€ für PKW’s und 100€ für LKW’s** genommen. Die Kaution wird **zurückgezahlt beim Rausfahren** während des Aufbautages. 
@@ -68,16 +69,14 @@ Sollte es Probleme mit dem Strom oder anderem geben, gebt eurem Account Manager 
 
 Wenn ihr Sachen wieder einladen möchtet in eure PKW’s/LKW’s, dann könnt ihr erst ab 18 Uhr in die jeweiligen Bereich reinfahren. Die Regeln vom Aufbautag gelten hier dementsprechend. 
 
-
 ## Ihr könnt auch eine PDF Datei mit allen logistischen Informationen für Aussteller runterladen. Bitte auf diesen Link klicken.
-
 
 [Logistikinformationen für Aussteller runterladen](https://res.cloudinary.com/ecommerceberlin/image/upload/v1578945741/Exhibitors/exhibitors%20manual%202020/ecommerce_berlin_exhibitors_guide_2020_.pdf)
 
 
 Regards, 
 
-E-commerce Berlin Expo Team
+{{$footer}}
 
 @endcomponent
 
