85085fdc5b752f4c74923f4727d14a08758746a6
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-01-16 23:06:33 +0100

    EBE5 competition prefix fixed

--- app/Mail/GeneralExhibitorEmail.php
+++ app/Mail/GeneralExhibitorEmail.php
@@ -111,10 +111,8 @@ class GeneralExhibitorEmail extends Mailable
 
         $this->accountUrl = "https://account.".$domain."/#/login?token=" . $this->participant->token;
        
-
-        // $this->trackingLink = $this->profileUrl . sprintf("?utm_source=th3rCMiM_%s&utm_medium=link&utm_campaign=promoninja&utm_content=raw", $this->participant->company_id);
-
-        $this->trackingLink = $this->profileUrl . sprintf("?utm_source=th4wOPiy_%s&utm_medium=link&utm_campaign=promoninja&utm_content=raw", $this->participant->company_id);
+        //EBE5
+        $this->trackingLink = $this->profileUrl . sprintf("?utm_source=th4x90iy_%s&utm_medium=link&utm_campaign=promoninja&utm_content=raw", $this->participant->company_id);
 
 
         if($this->event_manager && $this->participant->email !== $this->event_manager){
