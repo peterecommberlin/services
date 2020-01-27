93d2e0008278cf8d433fa4633d3a6327d286b08a
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-01-27 15:00:54 +0100

    party rsvp fixes

--- resources/views/emails/company/ebe5-party-rsvp-de.blade.php
+++ resources/views/emails/company/ebe5-party-rsvp-de.blade.php
@@ -2,38 +2,43 @@
 
 @component('mail::message')
  
-Hallo {{$profile->translate("[[fname]]")}},
+# Hallo {{$profile->translate("[[fname]]")}},
 
 Die E-commerce Berlin Expo steht in den Startlöchern.
-Wir wollen dich freundlicherweise auf die E-commerce Berlin Expo Networking Party aufmerksam machen. 
-Bitte klicke auf den Link und füge die Firmenvertreter hinzu, die der Party beiwohnen werden.
+
+Wir wollen dich freundlicherweise auf die **E-commerce Berlin Expo Networking Party** aufmerksam machen. 
+
+Bitte klicke auf den Link und füge die **Firmenvertreter hinzu, die der Party beiwohnen werden.**
 
 @component('mail::button', ['url' => $accountUrl])
 Sing In to add or edit
 @endcomponent
 
 
-Falls du planst, mehr als zwei Gäste auf die Party mitzunehmen, dann füge diese zur Gästeliste hinzu. Wir werden uns im Anschluss bei dir wegen der Rechnung melden für jedes weitere Ticket zu den bereits zwei vorhandenen. 
+Falls du planst, **mehr als zwei Gäste auf die Party mitzunehmen**, dann füge diese zur Gästeliste hinzu. Wir werden uns im Anschluss bei dir wegen der Rechnung melden für jedes weitere Ticket zu den bereits zwei vorhandenen. 
 
-Jedes weitere Ticket kostet 49€.
+**Jedes weitere Ticket kostet 49€.**
 
+@component('mail::panel')
 
-* **Informationen: 
-Datum: 12.02.2020
-Einlass: ab 19 Uhr 
-Awards Zeremonie: 20-21 Uhr 
-Networking, Drinks und Buffet: 21-23 Uhr 
-Ort: Spindler & Klatt, Köpenicker Str. 16-17, 10997 Berlin
+## Informationen: 
 
+Datum: **12.02.2020**
+
+**Einlass:** ab 19 Uhr
+
+**Awards Zeremonie:** 20-21 Uhr
+
+**Networking, Drinks und Buffet:** 21-23 Uhr 
+
+Ort: **Spindler & Klatt, Köpenicker Str. 16-17, 10997 Berlin**
+
+@endcomponent
 
 Gerne können Sie uns aber auch auf diese E-Mail antworten.
  
-
 Beste Grüße, 
 
 {{$footer}}
 
 @endcomponent
-
-
-
--- resources/views/emails/company/ebe5-party-rsvp-en.blade.php
+++ resources/views/emails/company/ebe5-party-rsvp-en.blade.php
@@ -5,37 +5,41 @@
 # Hello {{$profile->translate("[[fname]]")}},
 
 The E-commerce Berlin Expo 2020 is just around the corner.
+
 We’d like to kindly remind you about the E-commerce Berlin Expo 2020 Networking Party.
 
-Please use the link button below and add your company representatives who will attend the party:
+Please use the link button below and add your company representatives **who will attend the party**:
 
 @component('mail::button', ['url' => $accountUrl])
 Sing In to add or edit
 @endcomponent
 
+## ATTENTION!
 
-ATTENTION!
-If you plan to take more than 2 people to the party, please add them to the guest list and we will get in touch with you in relation to issue the invoice for any additional guest (above 2). 
+If you plan to take **more than 2 people** to the party, please add them to the guest list and we will get in touch with you in relation to issue the invoice for any additional guest (above 2). 
 
-Each additional ticket is 49€ worth.  
+** Each additional ticket (above 2 participants) is 49€ worth.**
 
+@component('mail::panel')
 
-* **Agenda:
-Date: 12th February 2020
-Entrance: 7 pm
-Awards ceremony: 8 pm - 9 pm
-Networking, drinks and flying buffet: 9 pm - 11 pm
-Location: Spindler & Klatt, Köpenicker Str. 16-17, 10997 Berlin
+## Agenda
 
+Date: **12th February 2020**
 
-If you have any questions feel free to contact your Account Manager.
+**Entrance**: 7pm
 
+**Awards ceremony:** 8pm - 9pm
+
+**Networking, drinks and flying buffet:** 9pm - 11pm
+
+Location: **Spindler & Klatt, Köpenicker Str. 16-17, 10997 Berlin**
+
+@endcomponent
+
+If you have any questions feel free to contact me - your Account Manager.
 
 Best Regards,
 
 {{$footer}}
 
 @endcomponent
-
-
-
