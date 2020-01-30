a6433f7e449504d0e2fe87d801b69be96da21ce8
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-01-30 13:56:40 +0100

    commands

---
+++ resources/views/emails/company/ebe5-promoninja-nopoints2-de.blade.php
@@ -0,0 +1,103 @@
+
+
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname") }},
+
+wir haben festgestellt, dass Euer Unternehmen nicht bei unserem Ausstellerwettbewerb teilnimmt. 
+
+** Alternativ kannst du diese Nachricht an Euer PR-Team weiterleiten. **
+
+Falls du eine **Präsentation** auf einer unserer 4 Bühnen haben möchtest oder extra Sichtbarkeit notwendig ist (**Logo auf den Besucherbadges** oder im **Videointerview**), dann ist der Wettbewerb der einfachste Weg zum Erfolg. 
+
+** Zur Zeit sind das die Top 3:**
+
+A4G DSP (629 Punkte), 
+
+knowhere (596 Punkte), 
+
+hello.de AG (520 Punkte).
+
+([vollständige Rangliste](https://account.ecommerceberlin.com/#/ranking))
+
+## Quickstart-Guide (Werbematerial mit deinem personaliserten Link)
+
+a) [Download einer fertigen ZIP-Datei mit einem E-Mail-Newsletter-Template.]({!! array_get($newsletter, "zip") !!})
+
+b) [Klick hier, um den Post auf LinkedIn zu teilen]({!! array_get($sharers, "linkedin") !!})
+
+c) [Klick hier, um den Post auf Facebook zu teilen]({!! array_get($sharers, "facebook") !!})
+
+d) [Klick hier, um den Post in deinem TT Account zu teilen]({!! array_get($sharers, "twitter") !!})
+
+## Würdest du mit uns einen 10-minütigen Call machen? 
+
+Dann erklären wir dir wie das Prozedere aussieht. Bitte [benutze den Link hier]({!! $calendar !!}) und wähle den für dich besten Zeitpunkt. 
+
+## Mehr Infos zum Thema
+
+Jeder Klick auf den Link beschert dir einen Punkt und Extrasichtbarkeit - da der **Verkehr zu deinem öffentlichen Profil** weitergeleitet wird. 
+
+Falls ihr Eure **eigenen Promomaterialien benutzen wollt**, dann könnt ihr das machen aber in Verbindung **mit dem personalisierten Link. **
+
+** Benutze es beim Erstellen von Newslettern, E-Mail-Signaturen, Werbungen (Faceboook ads, Adwords, Soziale Medien) oder beim direkten Kontakt mit euren Kunden. **
+
+@component('mail::panel')
+[{{ $trackingLink }}]({{ $trackingLink }}) 
+@endcomponent
+
+**  WICHTIG: Der Wettbewerb läuft bis zum 4.02.2020, 23:59 Uhr MEZ. **
+
+In deinem Ausstellerprofil wirst du 4 wichtige Sektionen finden in Bezug auf den Wettbewerb. 
+
+## RANKING
+
+Das Ranking basiert auf den verarbeiteten Daten von Google Analytics.
+
+## KRITERIEN
+
+Jeder Preis ist an bestimmte Kriterien geknüpft: 
+
+* eine bestimmte Anzahl an Punkten (gemessen an den Klicks des personalisierten Links) 
+* Position im Ranking 
+
+Du kannst deine Position im Ranking in Echtzeit beobachten, so dass du ggf. noch einen Einfluss auf das Resultat haben kannst. 
+
+## PREISE 
+
+**Exklusiv gebrandete Besucherbadges **
+
+Besucherbadges mit dem Logo des Gewinners. Die Badges werden an alle Besucher der E-commerce Berlin Expo verteilt (7k+Besucher).
+
+**Speaking Slot auf einer der Bühnen **
+
+Die Präsentation kommt in die Agenda und wird in den öffentlichen Kanälen promotet als Teil der Agenda der Expo. 
+
+**Videointerview**
+
+Dein Unternehmen wird auf eurem Messestand während der Expo interviewed! Du bekommst die Promomaterialien und wir werden das Interview nach der Expo promoten. 
+
+**Das Promoten Eures Logos auf unserer Website**
+
+Diese Preis führt dazu, dass Euer Logo auf unserer Website für die nächsten 11 Monate sichtbar sein wird - also bis zur Expo 2021. 
+
+@component('mail::button', ['url' => $accountUrl])
+Sign In
+@endcomponent
+
+
+## Willst du deine eigenen Promomaterialien im Wettbewerb benutzen? Das ist perfekt, nur vergiss dabei das Integrieren des personalisierten Links nicht. 
+
+@component('mail::panel')
+[{{ $trackingLink }}]({{ $trackingLink }}) 
+@endcomponent
+
+## Jeder Klick auf den Link bringt dir einen Punkt. Nur seriöse Klicks werden im Ranking beachtet. 
+
+
+Regards, 
+
+{{$footer}}
+
+@endcomponent
+
---
+++ resources/views/emails/company/ebe5-promoninja-nopoints2-en.blade.php
@@ -0,0 +1,103 @@
+
+
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname") }},
+
+(** You can forward this message to a person responsible for PR in your company. **)
+
+We noticed that you haven’t actively taken part in our Exhibitor Contest.
+
+If you want to **win a presentation slot** on one of our stages and gain some extra brand visibility (your **logotype on official badges**, **leaflets** distribution permission or **video interviews**), taking part in the Contest is the easiest way to do so!
+
+For now, the **top 3 companies are:** 
+
+A4G DSP (over 629 points), 
+
+knowhere (596 points), 
+
+hello.de AG (520 points).
+
+([Full ranking](https://account.ecommerceberlin.com/#/ranking))
+
+## Quickstart quide. Promo materials with your "tracking link"
+
+a) [Download a ready-to-send .zip package with an e-mail newsletter]({!! array_get($newsletter, "zip") !!})
+
+b) [Click here to share info on your Linkedin account]({!! array_get($sharers, "linkedin") !!})
+
+c) [Click here to share info on your Facebook account]({!! array_get($sharers, "facebook") !!})
+
+d) [Click here to share info on your Twitter account]({!! array_get($sharers, "twitter") !!})
+
+## Do you want to schedule a 10 min chat with us?
+
+...we can briefly **discuss how to use promotional materials**. Please use [this link]({!! $calendar !!}) below to choose the most suitable time for you.
+
+## More info
+
+Each page visit coming from above links grants you points, and extra visibility - since **traffic goes directly to your company public profile**. 
+
+If you would like to earn points using **your own promo materials**, you can do it with your customized link (plase find it below). 
+
+Use it when creating newsletters, e-mail footers, advertisement (FB ads, adwords, social media) or when directly informing your clients.
+
+@component('mail::panel')
+[{{ $trackingLink }}]({{ $trackingLink }}) 
+@endcomponent
+
+**  IMPORTANT: the competition is open until the 4th of February 2020, 11:59 PM CET.**
+
+In your Exhibitor’s account, you will find 4 important sections related to the Contest.
+
+## RANKING
+
+The ranking is created according to data fetched from Google Analytics.
+
+## REWARDS
+
+Each of the prizes requires meeting several criteria:
+
+- a minimum number of points (unique page views via your customized link) 
+- position in the ranking. 
+
+You can track your position almost in real-time, so it is good to keep an eye on it and make extra efforts if needed (getting additional points, extra promotion) since your position can be changed anytime. 
+
+# PRIZES
+
+**Exclusive branded visitor badges**
+
+Exclusive branded visitor badges with the Winner’s logo. The badges will be handed out to all of the visitors (7000+) during the E-commerce Berlin Expo 2020.
+
+**Presentation slot on one of our stages**
+
+30-minute presentation slot during the upcoming Expo. Your presentation will be added and promoted via the official agenda. 
+
+**VIDEO interview**
+
+Your company will be video-interviewed at your booth during Expo! You will receive the promotional material, we will promote it after the event as well.
+
+** Promotion of the company logo on our website**
+
+Your company’s logo on our official website, displayed for the next 11 months - until the EBE 2021.
+
+
+@component('mail::button', ['url' => $accountUrl])
+Sign In
+@endcomponent
+
+## Do you want to create and use your own promo materials in the Contest? That’s great - always use the provided link!
+
+@component('mail::panel')
+[{{ $trackingLink }}]({{ $trackingLink }}) 
+@endcomponent
+
+Each click coming from your link gives you one point. Only UNIQUE CLICKS through your customized link count towards the ranking. 
+
+
+Regards, 
+
+{{$footer}}
+
+@endcomponent
+
