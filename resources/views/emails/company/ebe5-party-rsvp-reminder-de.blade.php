b5e1ab033f6d1fdb349536378f41f2c71ff6a609
peterecommberlin <35766428+peterecommberlin@users.noreply.github.com>
2020-02-03 13:46:29 +0100

    rsvp reminder

---
+++ resources/views/emails/company/ebe5-party-rsvp-reminder-de.blade.php
@@ -0,0 +1,50 @@
+
+
+@component('mail::message')
+ 
+# Hallo {{ array_get($profile, "fname") }},
+
+Die E-commerce Berlin Expo steht in den Startlöchern.
+
+Wir möchten dich darüber informieren, dass die Anmeldefrist für die **E-commerce Berlin Expo Networking Party** am 5.02.2020 (Tagesende) zu Ende geht. Danach wird es nicht mehr möglich sein, Vertreter Eures Unternehmens auf die Gästeliste einzutragen.
+
+@component('mail::panel')
+
+## Informationen: 
+
+Datum: **12.02.2020**
+
+**Einlass:** ab 19 Uhr
+
+**Awards Zeremonie:** 20-21 Uhr
+
+**Networking, Drinks und Buffet:** 21-23 Uhr 
+
+Ort: **Spindler & Klatt, Köpenicker Str. 16-17, 10997 Berlin**
+
+@endcomponent
+
+## Bitte klicke auf den Link und füge die **Firmenvertreter hinzu, die der Party beiwohnen werden.**
+
+@component('mail::button', ['url' => $accountUrl])
+Sing In to add or edit
+@endcomponent
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/v1580134883/ebe_interface_party_de.png" style="margin-top: 10px; margin-bottom: 30px;" />
+
+Falls du planst, **mehr als zwei Gäste auf die Party mitzunehmen**, dann füge diese zur Gästeliste hinzu. Wir werden uns im Anschluss bei dir wegen der Rechnung melden für jedes weitere Ticket zu den bereits zwei vorhandenen. 
+
+**Jedes weitere Ticket kostet 49€.**
+
+@component('mail::button', ['url' => $accountUrl])
+Sing In to add or edit
+@endcomponent
+
+
+Gerne können Sie uns aber auch auf diese E-Mail antworten.
+ 
+Beste Grüße, 
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/ebe5-party-rsvp-reminder-en.blade.php
@@ -0,0 +1,52 @@
+
+
+@component('mail::message')
+
+# Hello {{ array_get($profile, "fname") }},
+
+The E-commerce Berlin Expo 2020 is just around the corner.
+
+We’d like to inform you about the deadline (February 5, 2020, EOD) to register your company representatives to ** the E-commerce Berlin Expo 2020 Networking Party**. After this date the list of party participants will be closed and there will be no option to add more people to the guest list.
+
+
+@component('mail::panel')
+
+## Agenda
+
+Date: **12th February 2020**
+
+**Entrance**: 7pm
+
+**Awards ceremony:** 8pm - 9pm
+
+**Networking, drinks and flying buffet:** 9pm - 11pm
+
+Location: **Spindler & Klatt, Köpenicker Str. 16-17, 10997 Berlin**
+
+@endcomponent
+
+## Please use the link button below and add your company representatives **who will attend the party**:
+
+@component('mail::button', ['url' => $accountUrl])
+Sing In to add or edit
+@endcomponent
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/v1580134883/ebe_interface_party_en.png" alt="" style="margin-top: 10px; margin-bottom: 30px;" />
+
+## ATTENTION!
+
+If you plan to take **more than 2 people** to the party, please add them to the guest list and we will get in touch with you in relation to issue the invoice for any additional guest (above 2). 
+
+** Each additional ticket (above 2 participants) is 49€ worth.**
+
+@component('mail::button', ['url' => $accountUrl])
+Sing In to add or edit
+@endcomponent
+
+If you have any questions feel free to contact me - your Account Manager.
+
+Best Regards,
+
+{{$footer}}
+
+@endcomponent
