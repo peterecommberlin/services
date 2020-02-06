b6b56c78a8fc32072e7f575a972e1e3bca780482
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-02-06 13:18:59 +0100

    update

--- resources/views/emails/company/ebe5-logistics-reminder-message-en.blade.php
+++ resources/views/emails/company/ebe5-logistics-reminder-message-en.blade.php
@@ -2,14 +2,13 @@
 
 @component('mail::message')
  
-# Hello {{$profile->translate("[[fname]]")}},
+# Hello {{ array_get($profile, "fname") }},
 
 Here you have basic logistic information about E-commerce Berlin Expo:
 
+Place:
 
-## Place:
-
-STATION Berlin  - Luckenwalder Str. 4-6, 10963 Berlin 
+## STATION Berlin  - Luckenwalder Str. 4-6, 10963 Berlin 
 
 ## Parking during assembly day (Wednesday / 12 Feb)
 
@@ -19,11 +18,11 @@ If your exhibition space is at **hall 1** (right floor on the map), you can park
 
 If your exhibition space is in **hall 2** (the left one on the map) you can park your car entering from **Trebbiner Str.**
 
-The venue owner will take a **deposit** for cars worth **50€** and for **trucks 100€**. The deposit will be given back when you leave the venue during the assembly day.
+The venue owner will take a **deposit** for cars worth **50€** and for **trucks 100€**.  The deposit will be given back when you leave the venue during the assembly day.
 
 <img src="https://res.cloudinary.com/ecommerceberlin/image/upload/w_650,c_fit/v1549965815/images/SBWEB_Grundriss_1024x768_151102_Loft.png" alt="map" style="max-width: 650px; " />
 
-## Parking during EXPO day (Thursday / 20 Feb)
+## Parking during EXPO day (Thursday / 13 Feb)
 
 Parking during the Expo day is **not possible on the area of the venue. **
 
@@ -33,7 +32,7 @@ The one-day-ticket is 10€ worth.
 
 ## Assembly day hours
 
-* 12th of February from 1:00 PM to 10:00 
+* 12th of February from 1:00 PM to 10:00 PM
 * 13th of February from 8:00 AM to 9:30 AM.
 
 After entering the hall you need to go to one of the two **information points for the exhibitors** (one info point per hall). Your Account Manager will await :)
@@ -45,8 +44,10 @@ There, you will get your exhibitor **welcoming package** which includes:
 * internet network access - login data will be included. 
 * contact numbers to the staff of Expo,
 
+** Remember, that standard power access is:**  
 
-** Remember, that standard power access is:**  a) only 500 Watts (for the categories Start-Up, Hot/Super Hot). b) The grand space has 1000 Watts. 
+* only 500 Watts (for the categories Start-Up, Hot/Super Hot). 
+* The grand space has 1000 Watts. 
 
 If you didn't order additional electric output for your stand devices with high electricity consumption e.g. coffee are not allowed at the stand.
 
@@ -61,7 +62,6 @@ In case of any problem (e.g., with electricity) you should go to the information
 13th of February. Disassembly is possible from 5.15 PM to 10.00 PM. If you need to upload your car with your stuff, you will be able to drive on the venue from 6.00 PM, not earlier. 
 
 The same rules are valid as mentioned in the assembly day section. 
-
 
 Regards, 
 
