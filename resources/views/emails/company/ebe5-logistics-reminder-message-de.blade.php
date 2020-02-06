8c21e75dbbdab8cee4785a90a8795dc9f5bee1b9
peterecommberlin <35766428+peterecommberlin@users.noreply.github.com>
2020-02-06 10:27:23 +0100

    wersja DE logistics

---
+++ resources/views/emails/company/ebe5-logistics-reminder-message-de.blade.php
@@ -0,0 +1,63 @@
+
+
+@component('mail::message')
+ 
+# Hello {{$profile->translate("[[fname]]")}},
+
+Hier findest du Infos rund um die Logistik für die anstehende E-commerce Berlin 2020 Expo.
+
+
+## Ort:
+
+STATION Berlin, Luckenwalder Str. 4-6, 10963 Berlin 
+
+## Parksituation: 
+
+Das Parken auf dem Gelände der Expo ist am Tag der Expo nicht möglich. 
+
+Neben dem Expogelände gibt es **ein Parkhaus**, welches benutzt werden kann. **Das Tagesticket kostet 10€.**
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/w_650,c_fit/v1549965815/images/SBWEB_Grundriss_1024x768_151102_Loft.png" alt="map" style="max-width: 650px; " />
+
+Ihr könnt am Aufbautag (12.02.2020) mit den Autos auf dem Gelände vor den Hallen parken.
+
+Wenn deine Ausstellungsfläche in der **ersten Halle** ist (rechts auf der Mappe), dann kannst du von der **Luckenwalder Straße** aus reinfahren und dort parken. 
+
+Wenn deine Ausstellungsfläche in der **zweiten Halle** ist (links auf der Mappe) dann kannst du von der **Trebbine Straße** aus reinfahren und dort parken. 
+
+Beim Reinfahren wird eine **Kaution iHv. 50€ für PKW’s und 100€ für LKW’s** genommen. Die Kaution wird **zurückgezahlt beim Rausfahren** während des Aufbautages. 
+
+## Aufbauzeiten:
+
+**12.02.2020 13-24 Uhr** und **13.02.2020 8-9:30 Uhr**
+
+Seid ihr in euren Hallen angekommen, begebt euch bitte zum Infopunkt. Es wird zwei geben (1x pro Halle). Dort wird euer Account Manager auf euch warten. Dort bekommt ihr euer Willkommensbag ausgehändigt, was folgendes enthält: 
+
+* Ausstellerpässe der registrierten Vertreter 
+* Catering Voucher 
+* WLAN-Zugriff (Login-Daten) 
+* Kontaktdaten des Expoteams vor Ort 
+
+Wichtig! Jede Flächenkategorie innerhalb der 9m2 haben 500W Strom. Die Grandkategorie hat 1000W. 
+
+Wenn du keinen zusätzlichen Strom gebucht  und du vor hast Hochleistungsgeräte, wie eine Kaffemaschine anzuschließen, dann ist das verboten.
+
+## Expotag: 
+
+13.02.2020 von 10-17 
+
+Sollte es Probleme mit dem Strom oder anderem geben, gebt eurem Account Manager bescheid. Die Kontaktdaten werden im Willkommensbage sein. 
+
+## Abbau: 
+
+13.02.2020 17:15-24:00 
+
+Wenn ihr Sachen wieder einladen möchtet in eure PKW’s/LKW’s, dann könnt ihr erst ab 18 Uhr in die jeweiligen Bereich reinfahren. Die Regeln vom Aufbautag gelten hier dementsprechend. 
+
+
+Regards, 
+
+{{$footer}}
+
+@endcomponent
+
