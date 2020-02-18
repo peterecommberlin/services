2c7d3cb7185951d90888b0e8846ade4ae765c06f
peterecommberlin <35766428+peterecommberlin@users.noreply.github.com>
2020-02-18 11:17:59 +0100

    2 wersje BD i 2 wersje JS
    bardzo prosze nie zmieniac podpisu, wyslac jako osoba trzecia lub Expo team z adresu hello. Tam jest specjalnie pytanie kto byl twoim accountem do wpisania z palca! tak ma byc.

---
+++ resources/views/emails/company/survey-en-js.blade.php
@@ -0,0 +1,23 @@
+
+
+@component('mail::message')
+ 
+# Hi {{$profile->translate("[[fname]]")}},
+
+**We have prepared a questionnaire**, thanks to which we hope to smooth out any sharp organizational edges in the nearest future :)
+
+Please find a few minutes and fill in.
+
+@component('mail::button', ['url' => "https://forms.gle/Z9GDgyj4Xi49afd26"])
+Complete the Survey
+@endcomponent 
+
+We have published selected photos from the event [on our Facebook fanpage](https://www.facebook.com/pg/ecommerceberlin/photos/?tab=album&album_id=3063981610281458) 
+
+**We encourage you to find and tag yourself and your friends.**
+
+
+Regards,
+E-commerce Berlin Expo Team
+
+@endcomponent
