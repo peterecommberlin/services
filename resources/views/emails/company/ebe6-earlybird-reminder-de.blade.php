6d0dfb4af739ff582df106533c02148407bf789b
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-03-03 15:29:59 +0100

    ebe6 earlybird reminder

---
+++ resources/views/emails/company/ebe6-earlybird-reminder-de.blade.php
@@ -0,0 +1,34 @@
+
+
+@component('mail::message')
+
+# Hi {{ array_get($profile, "fname") }},
+
+die E-commerce Berlin Expo 2020 gehört der Vergangenheit an, jedoch laufen die Vorbereitungen für die Expo 2021 (18.02.2021 @ STATION Berlin) auf Hochtouren. 
+
+# Wir möchten dich darüber informieren, dass am kommenden Donnerstag, **den 05.03.2020, ab 12 Uhr**, der Vorverkauf für die anstehende Expo beginnt. 
+
+Was sind die Vorteile? 
+
+* Zugang zu den Topflächen zum günstigsten Preis 
+* Sichere dir die Toplocation 
+* sichere dir den Topplatz vor der Konkurrenz 
+
+Die Preise beginnen ab 2500€ für 9m2 bis zu 5200€ für 18m2. 
+
+Der nachfolgende Link bringt euch zur Bookingseite. Dort werdet ihr die Preise sehen. 
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/exhibit"])
+Check the map here
+@endcomponent
+
+**WICHTIG!** Bitte beachte, dass der Vorverkauf bis zum 31.03.2020 offen ist. Danach werden die Preise steigen und du wirst nicht mehr die Möglichkeit haben, die Standfläche zu den oben genannten Konditionen zu bekommen. 
+
+Mit freundlichen Grüßen,
+
+Lucas Zarna 
+
+@endcomponent
+
+
+
---
+++ resources/views/emails/company/ebe6-earlybird-reminder-en.blade.php
@@ -0,0 +1,34 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+The E-commerce Berlin Expo 2020 is past but the preparations for the Expo 2021 (18.02.2021 @ STATION Berlin) have already started. 
+
+# We would like to inform you that on next Thursday, the **5th of March 2020, from 12:00 pm**, the Early Bird sales starts.
+
+What are the benefits of it? 
+
+* get access to the top locations on a lower price 
+* secure a premium spot 
+* be faster than your competitors 
+
+The fees will start from 2500€ for 9m2 up to 5200€ for 18m2. 
+
+The following link will show you the stands and pricing.
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/exhibit"])
+Check the map here
+@endcomponent
+
+**ATTENTION!** Please be aware that the Early Bird sales pool will be active until the 31st of March. Afterwards, the fees will increase and you won’t be able to book a spot on the above mentioned prices.
+
+Best regards,
+
+Lucas Zarna
+
+@endcomponent
+
+
+
