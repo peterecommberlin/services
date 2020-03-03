df1413cc09b3274bea5147c074ad62b14b8b84ee
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-03-03 13:43:46 +0100

    ebe6 top30 reminder fixes

--- resources/views/emails/company/ebe6-top30-earlybird-reminder-de.blade.php
+++ resources/views/emails/company/ebe6-top30-earlybird-reminder-de.blade.php
@@ -1,25 +1,23 @@
 
 
 @component('mail::message')
- 
-# Hello {{$profile->translate("[[fname]]")}},
 
-Wir möchten euch daran erinnern, dass Morgen die exklusive Salesrunde für die TOP30-Unternehmen aus dem letzten Ausstellerwettbewerb startet und **zwar ab 10 Uhr!**
+# Hello {{ array_get($profile, "fname") }} ,
 
-Die Reservierung der Standflächen wird bis zum Ende des Tages des 04.03.2020 möglich sein. 
+Wir möchten euch daran erinnern, dass **Morgen die exklusive Salesrunde** für die TOP30-Unternehmen aus dem letzten Ausstellerwettbewerb startet und **zwar ab 10 Uhr!**
+
+Die Reservierung der Standflächen wird **bis zum Ende des Tages des 04.03.2020 möglich sein. **
+
 Der Salesstart für alle anderen beginnt am 05.03.2020.
 
 Dieser Link führt euch zur Bookingseite wo ihr alle Standflächen sehen werdet. Die Buchungen können ab 10 Uhr vorgenommen werden.
 
-@component('mail::button', ['url' => "https://pages.ecommerceberlin.com/top30earlybird"])
+@component('mail::button', ['url' => "https://ecommerceberlin.com/top30earlybird_9f8s83jd"])
 Booking map, preise, buche hier
-
 @endcomponent
 
 
-**WICHTIG!**
-Buchungen über die Homepage werden nicht möglich sein. 
-
+**WICHTIG!** Buchungen über die Homepage werden nicht möglich sein. 
 
 Mit freundlichen Grüßen,
 
--- resources/views/emails/company/ebe6-top30-earlybird-reminder-en.blade.php
+++ resources/views/emails/company/ebe6-top30-earlybird-reminder-en.blade.php
@@ -2,22 +2,21 @@
 
 @component('mail::message')
  
-# Hello {{$profile->translate("[[fname]]")}},
+# Hello {{ array_get($profile, "fname") }} ,
 
-We would like to kindly remind you about the start of the exclusive sales round for TOP30 companies in the recent Exhibitors' Contest by tomorrow, **4th March 2020 from 10:00 AM**. 
+We would like to kindly remind you about the start of the exclusive sales round for TOP30 companies in the recent Exhibitors' Contest by **tomorrow, 4th March 2020 from 10:00 AM**. 
 
-The reservation of exhibition spaces will be available till the 4th March 2020 EOD! 
+The reservation of exhibition spaces will be available **till the end of the day - 23:59. (4th of March 2020)**
 
 The sales round for the other exhibitors (from 2020 edition) will be relesed on the 5th of March 2020.
 
 The link below will redirect you to the booking map where you will see all the exhibition spaces available. 
 
-@component('mail::button', ['url' => "https://pages.ecommerceberlin.com/top30earlybird"])
+@component('mail::button', ['url' => "https://ecommerceberlin.com/top30earlybird_9f8s83jd"])
 Map, prices, booking here
 @endcomponent
 
-**IMPORTANT**
-Reservations through other pages than linked above will not be possible.
+**IMPORTANT** Reservations through other pages than linked above will not be possible.
 
 Best regards,
 
