3b6a5e84459c224c974a815050de51bf6904f23d
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-03-04 10:48:05 +0100

    fix

--- resources/views/emails/company/ebe6-earlybird-reminder-de.blade.php
+++ resources/views/emails/company/ebe6-earlybird-reminder-de.blade.php
@@ -4,20 +4,17 @@
 
 # Hi {{ array_get($profile, "fname") }},
 
-Wir wollen dich auf den Beginn des exklusiven Early-Bird-Sales' aufmerksam machen, der nur für die Aussteller der letzten Expo vorgesehen ist. Dieser beginnt am Donnerstag, den 05.03.20 ab 12 Uhr.
-
-# Reservierungen in diesem Salespool sind nur bis zum 31.03.20 möglich. Danach gibt es keinen Early-Bird mehr und die Preise werden steigen.
-
+Wir wollen dich auf den Beginn des exklusiven Early-Bird-Sales' aufmerksam machen, der nur für die Aussteller der letzten Expo vorgesehen ist. **Dieser beginnt am Donnerstag, den 05.03.20 ab 12 Uhr.**
 
 Der Link unten wird dich zur Bookingseite weiterleiten wo du alle Standflächen sehen wirst. 
 
 @component('mail::button', ['url' => "https://ecommerceberlin.com/exhibit"])
-hier zur Bookingseite
+Hier zur Bookingseite
 @endcomponent
 
-**WICHTIG!** 
-Es kann sein, dass einige Flächen mit einem „R“ versehen sind, d.h diese Standfläche ist bereits vergeben, da die TOP30 aus dem Ausstellerwettbewerb diese bereits ergattert haben. Diese Flächen werden dann nicht mehr verfügbar sein. 
-Der Early-Bird-Verkauf wird bis zum 31.03 gültig sein. Danach wird es nicht mehr möglich sein, die Flächen zu den Preisen zu buchen. 
+**WICHTIG!** Es kann sein, dass einige Flächen mit einem „R“ versehen sind, d.h diese Standfläche ist bereits vergeben, da die TOP30 aus dem Ausstellerwettbewerb diese bereits ergattert haben. Diese Flächen werden dann nicht mehr verfügbar sein. 
+
+Reservierungen in diesem Salespool sind **nur bis zum 31.03.20** möglich. Danach gibt es keinen Early-Bird mehr und die Preise werden steigen.
 
 Mit freundlichen Grüßen,
 
--- resources/views/emails/company/ebe6-earlybird-reminder-en.blade.php
+++ resources/views/emails/company/ebe6-earlybird-reminder-en.blade.php
@@ -4,9 +4,7 @@
  
 # Hello {{ array_get($profile, "fname") }},
 
-We would like to kindly remind you about the **limited Early Bird sales round for Exhibitors of the previous edition of the E-commerce Berlin Expo by **tomorrow, 5th March 2020 from 12:00 PM**.
-
-# You can make reservations for the Early Bird price only until the end of the month (31.03.20). After this date, the Early Bird offer will expire and the prices for exhibition spaces will increase.
+We would like to kindly remind you about the **limited Early Bird sales round** for Exhibitors of the previous edition of the E-commerce Berlin Expo starts **tomorrow, 5th March 2020 from 12:00 PM**.
 
 The link below will redirect you to the booking map where you will see all the exhibition spaces available. 
 
@@ -14,9 +12,9 @@ The link below will redirect you to the booking map where you will see all the e
 Check the map here
 @endcomponent
 
-**ATTENTION!** 
-Please be aware that some of the spots are already booked and marked as "R" for companies from the recent Exhibitors' Contest. These exhibition space won't be available in the sales process. 
-The Early Bird sales pool will be active until the 31st of March. Afterwards, the fees will increase and you won't be able to book a spot on the above mentioned prices.
+**ATTENTION!** Please be aware that some of the spots may be already booked by "top30" winners of the recent Exhibitors' Contest and marked as "R". 
+
+You can make reservations for the Early Bird price only until the end of the month (31.03.20). After this date, the Early Bird offer will expire and the prices for exhibition spaces will increase.
 
 Best regards,
 
