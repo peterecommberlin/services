60e7fb408a8fef195e26428e9b4203aa7494f24e
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-03-18 15:12:16 +0100

    fix

--- resources/views/emails/company/teh18-postponed-en.blade.php
+++ resources/views/emails/company/teh18-postponed-en.blade.php
@@ -8,7 +8,7 @@ Nadchodzimy z informacja dotyczącą planowanej na kwiecień edycji Targów eHan
 
 Przepraszamy za długie trzymanie w niepewności - musieliśmy przeanalizować sytuację wielowymiarowo, co niestety jest czasochłonne.
 
-# Informacja w skrócie:
+## Informacja w skrócie:
 
 @component('mail::panel')
 
@@ -18,13 +18,13 @@ Wszelkie **rezerwacje przestrzeni** wystawienniczej, które dotyczyły planowane
 
 @endcomponent
 
-Pełna informacja:
+## Pełna informacja:
 
 **Rozporządzenie ministra zdrowia z dnia 14 marca** w sprawie ogłoszenia na obszarze Rzeczypospolitej Polskiej stanu zagrożenia epidemicznego, wprowadzające m.in. **zakaz zgromadzeń pow. 50 osób**, powoduje brak możliwości realizacji Targów eHandlu w dniu 22 kwietnia 2020 roku.
 
 Po analizie prognoz epidemiologów dotyczących prawdopodobnych scenariuszy rozwoju sytuacji, doszliśmy do wniosku, że jakakolwiek próba organizacji Targów w tym półroczu (utrzymanie lokalizacji za wszelką cenę i przesunięcie terminu np na połowę czerwca) obarczona jest zbyt dużym ryzykiem dla wszystkich.
 
-Z uwagi na **kompatybilność hal targowych** w Krakowie i Warszawie (metraż, proporcje ścian hal, rozkład kanałów technicznych) jesteśmy w stanie przenieść wszystkie rezerwacje gwarantując identyczną ekspozycję Wystawców względem kluczowych elementów Targów jak szlaki komunikacyjne, scena networkingu, sceny prezentacyjne, wejście na Halę. Zmiany będą wyłącznie kosmetyczne - dotyczące głównie szlaków ewakuacyjnych.
+Z uwagi na **kompatybilność hal targowych** w Krakowie i Warszawie (metraż, proporcje ścian hal, rozkład kanałów technicznych) jesteśmy w stanie **przenieść wszystkie rezerwacje** gwarantując **identyczną ekspozycję Wystawców** względem kluczowych elementów Targów jak szlaki komunikacyjne, scena networkingu, sceny prezentacyjne, wejście na Halę. Zmiany będą wyłącznie kosmetyczne - dotyczące głównie szlaków ewakuacyjnych.
 
 Jeśli uczestnictwo w Targach w terminie 23 października 2020 w Warszawie, z uwagi na inne plany, jest dla Państwa nieakceptowalne - prosimy o odpowiedź na wiadomość. Postaramy się znaleźć optymalne rozwiązanie.
 
