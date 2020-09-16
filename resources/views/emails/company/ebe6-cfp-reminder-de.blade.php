ac1c762be0f1c0a81455e129290c2c211ef53746
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-09-16 10:33:18 +0200

    ebe6 cfp email - de

---
+++ resources/views/emails/company/ebe6-cfp-reminder-de.blade.php
@@ -0,0 +1,49 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+We would like to kindly remind you about the ** E-commerce Berlin Expo - Call for Papers contest**. The submission process is already open and we are looking forward to receiving your submission.
+
+# Speaking at the E-commerce Berlin Expo 2021 will boost your personal profile, grow brand awareness and generate quality connections at one of the most senior retail events in Germany.
+
+We are looking for data-driven case studies that cover the following topics:
+
+> Tips on new technologies that really work – along with practical examples
+
+> Best practices in multi-channel
+
+> Actionable insights on the use of data
+
+> Examples of successful personalization and its impact on customer experience
+
+> Cross-border opportunities and successful expansion into foreign markets
+
+> Other practical examples of e-commerce related problems solved or successes achieved
+
+
+We strongly encourage the Service Providers to submit with a representative of an Online Store together - case study formula.
+
+
+To start submitting your presentation proposal just follow the link by pressing the button below.
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking"])
+Submit a presentation proposal
+@endcomponent
+
+**How does it work?** 
+
+The deadline for submitting proposals is 30.09.2020.
+
+Before you submit your proposal and start working on your presentation, please bear in mind that we’re not looking for sales pitches. Based on our experience, the audience is not interested in listening to them unless they’ve already known or researched your services or products, or unless you have a great case to show. 
+
+
+Best regards,
+
+Lucas Zarna
+
+@endcomponent
+
+
+
