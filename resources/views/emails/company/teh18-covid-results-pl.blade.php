c7e0cb2d5fa3d5be4ece67579f8186d8dd110a22
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-13 14:09:21 +0200

    covid results

---
+++ resources/views/emails/company/teh18-covid-results-pl.blade.php
@@ -0,0 +1,233 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname") }},
+
+Dziękujemy za odpowiedź na ankietę czy najbliższe Targi eHandlu powinny zostać przełożone czy realizowane (pomimo narzuconego limitu uczestników) zgodnie z planem 23 października.
+
+Oto wyniki (uzasadnienia publikujemy poniżej).
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650/v1602586681/teh18postponed.png" alt="zaproszenie od eDrone" style="margin: 10px auto;" />
+
+Zgodnie z treścią poprzedniej wiadomości (jej kopia na samym końcu), z uwagi na to że już raz nie mogliśmy przeprowadzić Targów (jak to ujął jeden z Wystawców, w wyniku "bad luck"), **możliwość przełożenia nadchodzącej edycji uzależniamy od gotowości Wystawców do współfinansowania tej decyzji** (wbrew niektórym opiniom, organizacja Targów to nie tylko najem hali i otwarcie jej w dzień montażu)
+
+**Na ten moment nasz zapasowy termin to 10 czerwca 2021 (Warszawa)**
+
+@component('mail::button', ['url' => "https://res.cloudinary.com/eventjuicer/image/upload/v1602587842/TargieHandlu-OswiadczenieWystawcy.pdf"])
+Oświadczenie Wystawcy, który chce przełożenia Targów
+@endcomponent
+
+Z uwagi na trzymanie wielu dostawców oraz Państwa w niepewności chcielibyśmy podjąć ostateczną decyzję możliwie szybko. Niezbędne są do tego podpisane oświadczenia, dlatego prosimy o ich **przesłanie maksymalnie do czwartku rano.**
+
+
+## Uzasadnienia firm, które NIE CHCĄ przekładania targów
+
+> Teraz albo nigdy
+
+> do kwietnia niewiele się zmieni, więc po co generować dodatkowe koszty
+
+> Mniejsza liczba zwiedzających, ale może bardziej "konkretna'". W dobie wirusa handel przez internet zyskuje wiele, więc możliwe, że wiele sprzedawców szuka dodatkowych rozwiązań.
+
+> w naszej firmie występuje sezonowość, w związku z powyższym kolekcja, którą posiadamy jest aktualna przed okresem świątecznym.
+
+> Poniesione koszty przygotowania do targów.
+
+> Uważam, że w odpowiedni sposób przeprowadzona selekcja osób, które będą mogły spotkać się z wystawcami może przerodzić się w odpowiednie rezultaty. Myślę, że dla większości liczy się jakość leadów a nie ilość leadów. Więc moim zdaniem powinniśmy ustalić i skupić się na tym w jaki sposób selekcjonować osoby które mogą stać się odwiedzającymi wystawców. I to jak najszybciej. 
+
+> Zdecydowanie chcielibyśmy wziąć udział w uczestnictwie w październiku, kto wie co będzie w II kwartale roku 2021. 
+
+> Jesteśmy startupem i to miał być nasz debiut i możliwość dotarcia do naszych klientów. Rozważaliśmy dwie opcje: Państwa targi i imprezę, która odbywała się miesiąc wcześniej, tańszą, ale mniej renomowaną. Postawiliśmy na jakość i doświadczenie. Dwie edycje tamtych targów się odbyły, a u Państwa wiosenna przełożona, a obecna pod znakiem zapytania. Można skwitować - bad luck, z drugiej strony jesienny nawrót nie jest żadnym zaskoczeniem - należało przez pół roku popracować nad wersją hybrydową jak zrobili inni. Taką wersję można zawsze zrobić tylko online, a nie przenosić targi w nieskończość, co jest rozwiązaniem loose-loose. Tracą Państwo i my. W tej cenie i w tym terminie wchodzi w grę tylko rozwiązanie hybrydowe - wtedy będzie bezpieczniej i należnym wolumenem.
+
+
+> Targi się nie odbyły w kwietniu tego roku więc powinny być zwrócone pieniądze. Nie widzę tu uzasadnienia dopłacania a tym bardziej nie rozumiem też co to znaczy rezerwacja. Rezerwacja jest wtedy kiedy opłaca się cześć kwoty. Zapłaciłem całą kwotę za miejsce z którego nie skorzystałem. Nowa edycja targów w kwietniu 2021 powinna odbyć się niezależnie od targów z roku 2020.
+
+
+> Spróbujmy. W II kwartale 2021 naszym zdaniem sytuacja nie będzie inna jak dzisiaj.
+
+> Zależy mi na targach w tym roku
+
+> Uważam, że jeszcze w miarę luźne warunki prawne dopóki pozwalają - warto wykorzystać. Potem może być ciężko cokolwiek zorganizować.
+
+> Ze względu na obecną sytuację, nie mamy możliwości zwiększenia budżetu na tą imprezę. Przeniesienie targów byłoby przez nas akceptowalne jedynie pod warunkiem utrzymania ceny, którą już Państwu zapłaciliśmy, pomimo nie ponoszenia przez Państwa firmę kosztów przeprowadzenia wydarzenia w pierwszej połowie tego roku.  
+
+
+## Uzasadnienia firm, które CHCĄ przełożenia Targów
+
+
+> Zgodnie z korespondencją. Boimy się o zdrowie naszych pracowników. 
+
+> Nie ma sensu w takich warunkach przeprowadzać targów, targi służą łatwością kontaktu a przy takich ograniczeniach szanse na to są znikome.
+
+> bylismy na targach [tu nazwa imprezy targowej] we wrzesniu (kiedy nie bylo jeszcze tak duzych obostrzen) i byla masakra, praktycznie 0 klientów
+
+> Uzasadniony interes ekonomiczny jest niestety niewspółmiernie niski do poniesionych kosztów, dlatego preferuję przeniesienie na nowy termin.
+
+
+> Nikt nawet nie chce odkupić obecnie stoiska od nas bo nie ma chętnych. Raczej lepiej dopłacić 25% i kiedy będą ludzie cośzrobić niż teraz na szybko przy małej frekewncji nawet obecna kwota się po prostu nie zwróci. 
+
+> Targi w ograniczonej formie nie będą tak efektywne.
+
+> Zbyt duże obostrzenia i tym samym mniejsza wartość promocyjna wydarzenia. 
+
+> wysoce prawdopodobna niska frekwencja sprawia ze trwanie przy obecnym terminie bedzie tylko strata czasu i pieniedzy.
+
+> Zainteresowanie targami może być znacznie mniejsze 
+
+> bezpieczniej
+
+> Ilość uczestników z innych targów w połowie września była już wtedy o połowę mniejsza. Zakładając obecną sytuację i żółtą strefę liczymy, że ilość gości będzie znikoma. Nikt w CEO nie chce ryzykować kwarantanny i "zawieszenia" firmy. Dodatkowo, ze względów wiekowych, również nie jesteśmy za uczestnictwem 23.10. Także jesteśmy za przeniesieniem ich na nowy termin w przyszłym roku.
+
+> Ze względu na wysokie ryzyko epidemiologiczne
+
+> ze względu na rosnące statystyki zakażeń, nie chcemy narażać pracowników na udział w evencie w obecnym czasie
+
+> Ze względu na rosnące statystyki zakażeń, nie chcemy ryzykować zdrowia pracowników, którzy mieliby wziąć udział w  evencie
+
+> W Q2 2020 miało miejsce ograniczenie restrykcji/mniej zakażeń, w chwili obecnej wręcz przeciwnie.
+
+> Naszym zdaniem, w obecnej sytuacji, nie ma sensu realizować wydarzenia, które nie będzie realizowało podstawowych celów jakie ma spełniać. Dodatkowa opłata 25% do negocjacji i ustalenia. 
+
+> aktualna sytuacja epidemiologiczna
+
+> Mała szansa na utrzymanie dotychczasowych restrykcji - raczej będzie coraz gorzej. 
+
+> W tym momencie nie mamy ani kogo wysłac na targi (ze strachu), boimy się też, że nie przełożą się na liczbę klientów.
+
+> Organizowanie w 2020 totalnie nie ma sensu. Rozumiem wasze położenie - nikt przez Świetami 1-11  nie bedzie chciał jechać aby ryzykować 
+
+> Nie chcę narażać pracowników na ryzyko zarażenia w najistotniejszym dla nas kwartale roku.
+
+> Sytuacja pandemiczna w kraju, duże ryzyko zakażeń
+
+> wolimy przesunąć termin, zamiast uczestniczyć w targach z kiepskimi wynikami
+
+> Covid
+
+> NIE ZGADZAMY SIĘ NA DOPŁATĘ 25%  WARTOŚCI STOISKA 
+
+> Ważne tu jest szybkie info jaka jest ostateczna decyzja, gdyż z każdym dniem podejmujemy kolejne kroki związane z organizacją targów, dokonujemy zakupów itd.
+
+> Na chwilę obecną sytuacja jest tak dynamiczna i zmienna w związku z koronawirusem, że jedyne co podpowiada rozsądek, to przełożenie imprezy na nowy bezpieczny termin. 
+
+> Zbyt duże ryzyko zachorowania, a także szacowane zbyt małe zainteresowanie ze strony potencjalnych klientów, a tym samym brak zwrotu z inwestycji.
+
+> Pierwsza opcja (przeniesienie rezerwacji na wiosnę 2021) jest rozsądniejszym wyborem z uwagi zagrożenie epidemiologiczne i ograniczenia związane z organizacją wydarzeń targowych (ograniczona ilość osób). Dopłata ze strony wystawców jest także fair wobec organizatorów w tej trudnej dla wszystkich sytuacji. 
+
+> zakładam, że sytuacja pandemiczna będzie stabilniejsza niż obecnie
+
+> Zbyt niebezpieczne - teraz brac udzial w targach, ponadto - frekfencja moze byc rowniez zbyt mala w stosunku do kosztow poniesinych na targach. Bezpieczenstwo pracownikow i tak naprawde calosc przygotowan do targow gdzie dzien przed moze sie okazac ze Warszawa bedzie strefa czerwona i wtedy - calosc przygotowan pojdzie na marne. Stad decyzja o wycofaniu sie z imprezy - i przeniesienia na inny termin. Preferuje - powinno zawierac 3 opcje - calkowitej rezygnacji 
+Bardzo nam sie nie podoba - ze sa tylko narzucone 2 opcje bez mozliwosci rezygnacji z targow . ( bedziemy to monitorowac) 
+
+> Nasi handlowcy boją się uczestniczyć w targach 
+
+> E-commerce radzi sobie nieźle, nie dokładajmy cegiełki do covidowego szaleństwa jeśli to nie jest konieczne.
+
+> Nasza firma jest ulokowana w Czechach i to właśnie z Czech będziemy musieli przyjechać na event. W związku z tym, że nie wiadomo jak będą wyglądać restrykcje rządu za tydzień czy za dwa, preferujemy przeniesienie targół na następny rok. 
+
+> Edycja tegoroczna może przynieść nam wystawcom jedynie straty związane z transportem i kosztami targowymi, które już opłaciliśmy. Nie można przewidywać, że uczestnicy pojawią się na wydarzeniu, ponieważ wprowadzone obostrzenia zniechęcają do takich aktywności. Dodatkowo kwestie zdrowotne - mimo wprowadzonych środków bezpieczeństwa wystawcy i uczestnicy nie będą czuć się bezpiecznie. 
+
+> Na targach tego typu zależy nam na jak najszerszym bezpośrednim kontakcie z potencjalnymi klientami. Ograniczenia ilościowe uczestników ograniczają automatycznie ilość kontaktów i pozyskanie leadów. No i bezpieczeństwo oczywiście najważniejsze,
+
+> Targi i tak się nie odbędą za dużo chorych a rokowania na poprawę sytuacji bardzo złe. Nie widzę innej opcji jak przeniesienie targów. Nie ma co czekać do ostatniej chwili i być w niepewności odnośnie decyzji Rządu. 
+
+> Najlepiej obecnie to zwrocic wplaty i co bedzie na wiosne to nie wiadomo.
+
+> 1000 uczestników to niestety bardzo mało. Obawiamy się, że inwestycja się nie zwróci, zwłaszcza specjalnie na tę okazję będziemy zamawiać materiały marketingowe. 
+
+> Zważywszy na wciąż rosnący wzrost zachorowań, możemy spodziewać się dodatkowych obostrzeń, a co za tym idzie, odwołania Targów, albo przynajmniej ograniczenia liczby uczestników do tak małej ilości, że wystawienie się na Targach nie będzie dla nas opłacalne.
+
+> Przeniesienie terminu jest dla nas korzystniejsze, gdyż w przypadku braku zwiedzających udział w nich jest dla nas nieco pozbawiony sensu. 
+
+> Chciałem dać drugą odpowiedz - w mailu było B, a tu ta opcje jest pierwsza :) Żeby Wam nie zaburzyło odpowiedzi... Co do treści. Raczej komentarz zbędny. Teraz będą sami wystawcy, a wiemy że rezerwacje kosztują.
+
+> Lepiej będzie zorganizować event w 2021. Zadbajmy o siebie. Pozdrawiam [Imię i Nazwisko]
+
+> Jesteśmy za przelozeniem edycji na pozniejszy termin z uwagi na sytuacje epidemiologiczna, jednak nie do konca rozumiemy koniecznosc doplaty 25%, zwlaszcza jesli targi mialyby odbyc sie w Krakowie. 
+
+> Sytuacja epidemiologiczna w kraju spowoduje, że październikowe targi nie będa cieszyć się, z naszje perspektywy dużym zainteresowaniem, poza tym nie chcemy narażać naszych pracowników
+
+> Wybór padł na opcje przeniesienia rezerwacji. Aktualnie ograniczona liczba uczestników to tak jak Państwo wspomnieli " o wiele gorszy zwrot z inwestycji" 
+
+> Myślę, że taki wybór będzie bardziej optymalny. 
+
+> Our team is located abroad. The company's top-management will not risk the health of its employees in order to participate in the conference.
+
+
+> Bezpieczeństwo zwiedzających, jak i wystawców, a także brak opłacalności biznesowej podczas jesiennej edycji targów w 2020 roku.
+
+> W obecnym terminie weźmie udział niewielka liczba odwiedzających.
+
+> Obawiamy sie głównie słabego zwrotu z Targów, ze względu na mocno ograniczoną ilość odwiedzających. Również ze względów zdrowotnych nie mamy chetnych do udziału w Targach i obawiamy się o zdrowie nasze i naszych pracowników.
+
+> Jesteśmy za przeniesieniem terminu targów na przyszły rok ponieważ zdrowie naszych pracowników oraz zwiedzających jest najważniejsze oraz uważamy, że w obecnej sytuacji frekwencja, a co za tym idzie potencjalna korzyść z uczestnictwa w targach, będzie minimalna. Jednocześnie nie zgadzamy się z dodatkową opłatą 25% za przeniesienie terminu, ponieważ jest to nasz pierwszy przełożony termin targów ( sytuacja z przeniesieniem targów z Krakowa z początku roku nas nie dotyczy) oraz brak takich zapisów w regulaminie. Jako wystawca też ponieśliśmy już koszty związane z wykonaniem stoiska, materiałów reklamowych itd. Mając na uwadze naszą przyszłą współpracę proszę o pozytywne rozpatrzenie naszej prośby.
+
+> Ilość odwiedzających jest zbyt mała w porównaniu do kosztów wydarzenia i potencjalnego ryzyka zarażenia. Dziękuję i pozdrawiam
+
+> Obecnie obawiam się, że sporo ludzi zrezygnuje z przyjścia na targi
+
+> Organizacja targów w obecnej sytuacji, nawet przy zachowaniu nowych wytycznych, jest naszym zdaniem zbyt ryzykowna. Nie będziemy narażać naszego zespołu i tym samym nie pojawimy się na targach wcale. Myślę również, że tak samo postąpi wielu innych wystawców co spowoduje, że będziecie Państwo mieli pustą hale. Przeniesienie targów z późniejszą dopłatą jest najlepszym rozwiązaniem. 
+
+> Po pierwsze i najważniejsze - nie jesteśmy w stanie skompletować zespołu na targi. Pracownicy nie są chętni do narażania własnego zdrowia takim wyjazdem. Od początku pandemii robimy wszystko by zapewnić bezpieczne warunki pracy i taki wyjazd naraża nas wszystkich. Po drugie - koszt targów jest bardzo duży - ta edycja nie zwróci go nawet w części. Obostrzenia wpłyną na ilość odwiedzających a tym samym nie będziemy w stanie zrealizować celów biznesowych. Wizerunkowych i innych także nie. 
+
+> teraz nie przyjdzie nawet 1000 osób
+
+> w tym momencie w naszej opinii organizowanie targów będzie nieodpowiedzialne.
+
+> II kwartał 2021 daje szansę na polepszenie sytuacji epidemicznej. Aktualna sytuacja epidemiczna w Polsce nie wróży dobrej frekwencji dla targów w formule stacjonarnej.
+
+> Obawa przed Covidem przy jednocześnie niskiej frekwencji uczestników
+
+Dziękujemy za wsparcie i wyrozumiałość.
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+
+
+## Nasza poprzednia wiadomość
+
+
+W związku z wprowadzeniem w całym kraju tzw. "źółtej strefy", która narzuca dodatkowe obostrzenia także względem Organizatorów imprez takich jak Targi - m.in. limit 1 osoby na każde 4m2 powierzchni Targowej z (wyłączeniem obsługi) zwracamy się do Ciebie z ankietą.
+
+**Zanim ją wypełnisz prosimy Cię o poświęcenie chwili na zapoznanie się z naszą sytuacją.**
+
+Kwietniowe Targi w Krakowie nie mogły się odbyć z uwagi na "lockdown" całego kraju i prawny zakaz organizacji tego typu wydarzeń. Pragnąc utrzymać jak najlepsze relacje z naszymi Wystawcami dokonaliśmy bezkosztowego przeniesienia rezerwacji na Warszawę. Takie rozwiązanie wydawało nam się wtedy najlepsze. 
+
+Lepsze niż podpieranie się "siłą wyższą", regulaminem Targów czy nawet przepisami Kodeksu Cywilnego i anulowanie rezerwacji bez zwrotu płatności lub ze zwrotem uwzględniajacym uzasadnione koszty poniesione przez Organizatora (jeśli by patrzeć tylko na przepisy Kodeksu Cywilnego).
+
+Aktualna sytuacja epidemiczna w Polsce i obowiązujący stan prawny umożliwiają wprawdzie realizację zaplanowanych Targów ale mocno ograniczą organizacyjny rozmach, do którego przyzwyczailiśmy naszych Wystawców, Prelegentów i Zwiedzajacych. Na halę będzie mogło wejść niewiele więcej niż 1000 Zwiedzających czyli od 3 do 4x mniej niż w poprzednich edycjach. 
+
+Dochodzą do nas głosy, że oprócz obaw związanych ze zdrowiem, biorąc pod uwagę całościowe koszty związane z logistyką wystawiania się boicie się Państwo o o wiele gorszy "zwrot z inwestycji". 
+
+Naszym zadaniem jest zweryfikować zasadność takich głosów konfrontując je z zdaniem wszystkich, których sytuacja dotyczy.
+
+**Na ten moment rozważamy 2 opcje:**
+
+A) realizację Targów zgodnie z planem i dostosowanie ich do świeżo ogłoszonych ograniczeń (odmówienie części zarejestrowanym uczestnikom wejścia)
+
+B) zmianę terminu na II kwartał 2021 (lokalizacja Kraków bądź Warszawa). Wtedy 23/10 nagralibyśmy tylko prezentacje Prelegentów.
+
+opcji (B) nie będziemy mogli przeprowadzić bez zgody większości Wystawców i woli dopłaty w wysokości 25% wartości przestrzeni wystawienniczej na ok. miesiąc przed kolejnymi Targami (nie jesteśmy w stanie po raz kolejny bezkosztowo przenieść rezerwacji)
+
+**Która opcja jest dla Twojej firmy najlepsza?**
+
+@component('mail::button', ['url' => "https://forms.gle/L3LAxwRWQ7WTKh146"])
+Link do ankiety
+@endcomponent
+
+Jesteśmy również otwarci na ewentualne inne pomyśly - może istnieją jeszcze jakieś inne rozwiązania, które jeszcze lepiej realizują kompromis interesów wszystkich stron.
+
+
+Dziękujemy za wsparcie i wyrozumiałość.
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+@endcomponent
