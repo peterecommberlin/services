12243e6d98c2f4601b0210dc7832381b1f8cef05
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-16 14:34:02 +0200

    commands

--- resources/views/emails/company/teh18-red-zone-pl.blade.php
+++ resources/views/emails/company/teh18-red-zone-pl.blade.php
@@ -4,12 +4,14 @@
  
 # Cześć {{ array_get($profile, "fname") }},
 
-Od najbliższej soboty Warszawa znajdzie sie w tzw. "czerwonej strefie". Niezależnie od woli Wystawców i Organizatora, Targi nie będą mogły się odbyć.
+Od najbliższej soboty Warszawa znajdzie sie w tzw. "czerwonej strefie". Niezależnie od woli Wystawców i Organizatora, Targi nie będą mogły się odbyć. 
 
 W poniedziałek planujemy rozesłać do Państwa stosowne wzory oświadczeń:
 
-- dot. rozwiązania umowy
-- dot. zaliczenia opłaty na poczet kolejnej edycji Targów eHandlu (data robocza to 10 czerwca 2021 - Warszawa)
+•  dot. warunków rozwiązania umowy
+
+•  dot. warunków zaliczenia opłaty na poczet kolejnej edycji Targów eHandlu (data robocza to 10 czerwca 2021 - Warszawa)
+
 
 Z pozdrowieniami,
 
