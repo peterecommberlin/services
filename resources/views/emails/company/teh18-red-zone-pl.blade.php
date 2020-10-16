280e29f6225e26f1f6e0db80067c96d73be8f541
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-16 14:24:20 +0200

    redzone

---
+++ resources/views/emails/company/teh18-red-zone-pl.blade.php
@@ -0,0 +1,20 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname") }},
+
+Od najbliższej soboty Warszawa znajdzie sie w tzw. "czerwonej strefie". Niezależnie od woli Wystawców i Organizatora, Targi nie będą mogły się odbyć.
+
+W poniedziałek planujemy rozesłać do Państwa stosowne wzory oświadczeń:
+
+- dot. rozwiązania umowy
+- dot. zaliczenia opłaty na poczet kolejnej edycji Targów eHandlu (data robocza to 10 czerwca 2021 - Warszawa)
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+@endcomponent
