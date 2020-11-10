f6b4fc51ba30ad715a3659ea82492ee731113022
peterecommberlin <35766428+peterecommberlin@users.noreply.github.com>
2020-11-10 12:35:59 +0100

    mailing EGA dla wystawców

---
+++ resources/views/emails/company/ebe6-ega-info-exhibitors-en.blade.php
@@ -0,0 +1,47 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+We are happy to announce that submissions for the 4th edition of the E-commerce Germany Awards are already open. Now you can nominate your company for one of the prestigious contest for e-commerce service providers in Germany. 
+
+# As our exhibitor, you can take part in the contest completely free of charge and increase your visibility in the e-commerce industry! 
+
+Submit your application by November 29, 2019  - in one of the 10 different categories:
+
+>Best Sales Generating Tool 
+>Best Customer Communication Tool
+>Best Solution for International Expansion
+>Best Logistics Solution
+>Best Platform / Shop Software
+>Best Payment Provider
+>Best Analytics / BI Solutions
+>Best Agency
+>Best IT and infrastructural solution
+>Best innovation / New development in E-commerce
+
+
+
+To submit an entry, please complete and save the online entry form.
+
+@component('mail::button', ['url' => "https://ecommercegermanyawards.com/?utm_source=email&utm_medium=exhibitors&utm_campaign=awards_system_1"])
+Submit your company!
+@endcomponent
+
+
+
+Add a business trip to Berlin to your event calendar today! Remember that as an exhibitor, you have two tickets to the E-commerce Germany Awards 2020 ceremony included in your exhibitor package. The Gala will be a part of the pre-event party taking place on the 12th of February 2020. 
+
+
+
+Best regards,
+
+TEST
+
+Lucas Zarna
+
+@endcomponent
+
+
+
