860041b56c881a1601103d9456fb707cf5258fab
peterecommberlin <35766428+peterecommberlin@users.noreply.github.com>
2020-11-30 14:47:42 +0100

    Create ebe6-new-date-released.php
    Subject: Postponement of the E-commerce Berlin Expo 2021 / New date released
    
    grafika z podpisami - https://res.cloudinary.com/ecommerceberlin/image/upload/v1606743599/images/podpisy_mt_psz.png

---
+++ resources/views/emails/company/ebe6-new-date-released.php
@@ -0,0 +1,35 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+Due to the current situation with Covid-19, and in light of the current health and major events regulations that are in force, the overall development of the sanitary crisis and the quality standards regarding the number and type of attendees that we have been striving towards for months are no longer guaranteed.
+
+# To protect the trust and funds already invested in our event by our respective partners, we have decided to postpone the event until May 27th, 2021.
+
+From a business perspective, the situation is obviously less than ideal at the moment. We strongly believe that we would be able to organize an event with all of the current restrictions, but we are worried about the number of people who would attend it in February. We have always prioritized health and the highest organizational standards above all else, but the limitations on indoor event attendees in Q1 2021 might affect your ROI/outcome of the event.
+
+
+We’ve talked with a lot of companies, and a huge group of our current and potential exhibitors are only interested in a physical version of the event. We know how important face to face meetings are for all exhibitors, and we'd like to deliver the highest quality event possible with the most fruitful talks during the show!
+
+In order for this to happen and to meet participants’ expectations, we'd like to persevere with the initially proposed version of this event. Hybrid or online versions are therefore not an acceptable substitute in our case.
+
+In relation to the above mentioned and the decision about postponing the main project E-commerce Berlin Expo to May 27, 2021, we have also decided to extend the deadline for submissions in the E-commerce Germany Awards. 
+
+**The new deadline for submissions is 8 January 2021!**
+
+We thank you for your continued support and wish you the best for the coming months.
+Looking forward to seeing you in May next year!
+
+Best regards,
+
+Peter Szczepaniak & Mark Tomaszewski
+Co-Founders of the E-commerce Berlin Expo
+
+<LINK DO GRAFIKI Z PODPISAMI ZNAJDUJE SIĘ W KOMENTARZU DO PULLa>
+
+@endcomponent
+
+
+
