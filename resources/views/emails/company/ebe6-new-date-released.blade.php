bf79994cb23ded0c2f177ceb63ff150226d4db7b
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-12-01 13:31:29 +0100

    fixes

---
+++ resources/views/emails/company/ebe6-new-date-released.blade.php
@@ -0,0 +1,33 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+Due to the current COVID-19 restrictions on organizing trade fairs and conferences, people's consternation about the course of the COVID-19 pandemic, **we have decided to postpone the event to May 27th, 2021.**
+
+We strongly believed that we were capable of organizing the event with all applicable restrictions, but the concerns about the number of people who would attend when organized in February, our commitment to prioritizing health of attendees, highest organizational standards and overall outcome (ROI) of the event got us to make this decision.
+
+In addition to that, we’ve talked to many of current exhibitors and companies interested in participating as exhibitors and they were only interested in a physical/on-site formula of the event. We know how important and fruitful are face-to-face meetings for all parties therefore hybrid or digitial events may not bring expected outcome.
+
+In order for this to happen and to meet participants’ expectations, **we'd like to sustain the initially proposed formula of our event. Hybrid or online versions are therefore not an acceptable substitute in our case.**
+
+In relation to the above mentioned and the decision about postponing our main project - E-commerce Berlin Expo - to May 27, 2021, **we have also decided to extend the deadline for submissions to the E-commerce Germany Awards until January 8, 2021 **
+
+We thank you for your continued support and wish you the best for the coming months.
+
+Looking forward to seeing you in May next year!
+
+Best regards,
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/v1606743599/images/podpisy_mt_psz.png" alt="signatures" style="max-width: 250px;">
+
+Peter Szczepaniak & Mark Tomaszewski
+
+Managing Partners, E-commerce Berlin Expo
+
+
+@endcomponent
+
+
+
