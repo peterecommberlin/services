03a4b8a1b55561c5f53669fa5a5f115bbd1ac8b1
peterecommberlin <35766428+peterecommberlin@users.noreply.github.com>
2020-12-17 16:51:50 +0100

    christmas wishes
    subject:
    
    DE: Das Jahr 2020 geht zu Ende und wir möchten Ihnen sagen, dass...
    
    EN: 2020 is ending and we would like to tell you that...

---
+++ resources/views/emails/company/ebe6-christmas-message-de.blade.php
@@ -0,0 +1,22 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+Das Jahr 2020 neigt sich dem Ende zu und man kann sagen, dass dieses Jahr in vielerlei Hinsicht für jeden von uns eine kräftezehrendes, herausforderndes und vor allem spannendes Jahr unter verschiedensten Aspekten war. 
+
+Trotz der massiven Veränderungen, die 2020 mit sich getragen hat sagen wir uns trotz allem jedes Jahr im Dezember frohe Weihnachten und einen guten Rutsch ins neue Jahr - so auch dieses Jahr. 
+
+Wir möchten uns für eine partnerliche und gute Zusammenarbeit in diesem Jahr bedanken. Ihr seid das Fundament auf dem wir aufbauen und wir hoffen alle, dass das Jahr 2021 - trotz aller bevorstehenden Umstände - ein besseres Jahr für uns alle wird. 
+
+**In diesem Sinne frohe Weihnachten und einen guten Rutsch ins neue Jahr 2021!**
+
+Euer 
+E-commerce Berlin Expo Team
+
+
+@endcomponent
+
+
+
