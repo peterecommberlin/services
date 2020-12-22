7b9128d3af24a84e5038b51703f4ae4484049617
peterecommberlin <35766428+peterecommberlin@users.noreply.github.com>
2020-12-22 12:35:42 +0100

    christmas wishes (#20)
    * christmas wishes
    
    subject:
    
    DE: Das Jahr 2020 geht zu Ende und wir möchten Ihnen sagen, dass...
    
    EN: 2020 is ending and we would like to tell you that...
    
    * comit
    
    xx
    
    * christmass mailing v2
    
    DE Das Jahr 2020 geht zu Ende und wir möchten Ihnen sagen, dass ...
    EN: 2020 is ending and we would like to tell you that...

---
+++ resources/views/emails/company/ebe6-christmas-message-en.blade.php
@@ -0,0 +1,22 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+The year 2020 is slowly coming to an end and we can all say ‘finally!’ as it has been an exhausting, challenging and in many aspects exciting year.
+
+Though 2020 has brought many changes to our personal and professional lives and has influenced many areas of our lives let us keep one thing the same as it always was - us wishing merry Christmas and a happy new year to one another!
+
+We want to thank you for your partnership and collaboration in 2020.
+
+**With this in mind,we wish Merry Christmas and a Happy New Year 2021 to you, your team and your families!**
+
+
+Your E-commerce Berlin Expo Team
+
+
+@endcomponent
+
+
+
